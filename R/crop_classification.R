7b9c6ea47fa314fee35f4ed89f31d3d67fbb1f87
Gerard <gerard.ros@nmi-agro.nl>
2019-07-27 11:00:35 +0200

    function for crop sensitivity classes PKS

---
+++ R/crop_classification.R
@@ -0,0 +1,144 @@
+#' Determine classification rules for crops used to prepare crops.obic
+#' 
+#' This function determines crop classes given crop response to P, K and S fertilizers
+#' 
+#' @param B_LU_BRP (numeric) The crop code (gewascode) from the BRP
+#' @param B_BT_AK (character) The type of soil
+#' 
+#' @import data.table
+#' 
+#' @export
+calc_cropclass <- function(B_LU_BRP,B_BT_AK) {
+  
+  crop_category = crop_code = soiltype = id = NULL
+  
+  # Load data
+  crops.obic <- as.data.table(OBIC::crops.obic)
+  setkey(crops.obic, crop_code)
+  soils.obic <- as.data.table(OBIC::soils.obic)
+  setkey(soils.obic, soiltype)
+  
+  # Check inputs
+  arg.length <- max(length(B_LU_BRP), length(B_BT_AK))
+  checkmate::assert_numeric(B_LU_BRP, any.missing = FALSE, min.len = 1, len = arg.length)
+  checkmate::assert_subset(B_LU_BRP, choices = unique(crops.obic$crop_code), empty.ok = FALSE)
+  checkmate::assert_character(B_BT_AK, any.missing = FALSE, min.len = 1, len = arg.length)
+  checkmate::assert_subset(B_BT_AK, choices = unique(soils.obic$soiltype), empty.ok = FALSE)
+  
+  # Collect the data into a table
+  dt <- data.table(
+    id = 1:length(B_LU_BRP),
+    B_LU_BRP = B_LU_BRP,
+    crop_sensitivity_p = NA_real_,
+    crop_sensitivity_k = NA_real_,
+    crop_sensitivity_s = NA_real_
+  )
+  dt <- merge(dt, crops.obic[, list(crop_code, crop_category)], by.x = "B_LU_BRP", by.y = "crop_code")
+  dt <- merge(dt, soils.obic[, list(soiltype, soiltype.n)], by.x = "B_BT_AK", by.y = "soiltype")
+  setorder(dt, id)
+  
+  # Determine crop classication for P ----
+  
+  # lower case and character crop names
+  dt[,crop_name := tolower(as.character(crop_name))]
+  
+  # crop name selection strings for phosphorus
+  cr0 = 'andijv|augurk|bleeksel|raap|paksoi|pastin|peterselie|selderij|^sla|snijbiet|spinaz|venkel|kroten|chinese kool'
+  cr1 = 'aardappel|augurk|boon|erwt|bonen|knofl|koolrabi|knolselderij|peul|rammenas|spruitkool|uien|zaaiui'
+  cr2 = 'suikerbiet|bieten, suiker|bieten, voeder|voederbiet|zaadbiet|vlas|karwij|raapsteel|radicchio|radijs'
+  cr3 = 'bloembol|gladiool|krokus|amaryl|narcis|tulp|lelie|gerst|klaver|wikke|luzerne|cichorei|pastinaak'
+  cr4 = 'granen|hennep|haver|sorghum|triticale|spelt|graszoden|tarwe|spitskool|rogge|grasza|witte kool|koolza|aardbei|asperg|biesl|witte kool|bloemko|boerenko|rodekool|savooi|broccol|bladkool|courget|koolra|kroot|pompo|prei|rabarber|schorse|sluitko'
+  nat = 'bos|rand|griend|natuur|houtwal|onbeteeld|onbekend|struweel|oever|riet|wandel|wilg|brandn|cultuur|poel|bomenrij'
+  bkw = 'bomen|peren|bloem|rozen|facelia|hop|lupine|kers|bramen|heesters|elzen|pruimen|zonne|conifer|kruid|vaste|heg|boom|wijn|bloemkwek|heesters|appelen|buxus|bes'
+  grs = 'grasland|raaigras|rietzwenkgras|veldbeemdgras'
+  sms = 'mais|corncob|snijmai|maÃ¯s'
+  
+  # distinghuish P sensitivity crop classes
+  dt[grepl(grs,crop_name),crop_sensitivity_p := 'gras']
+  dt[grepl(sms,crop_name),crop_sensitivity_p := 'mais'] 
+  dt[grepl(cr0,crop_name),crop_sensitivity_p := 'class0']
+  dt[grepl(cr1,crop_name),crop_sensitivity_p := 'class1']
+  dt[grepl(cr2,crop_name),crop_sensitivity_p := 'class2']
+  dt[grepl(cr3,crop_name),crop_sensitivity_p := 'class3']
+  dt[grepl(cr4,crop_name),crop_sensitivity_p := 'class4']
+  dt[grepl(nat,crop_name),crop_sensitivity_p := 'natuur'] 
+  dt[grepl(bkw,crop_name),crop_sensitivity_p := 'bkw']
+  dt[grepl('witlof|peen',crop_name) & grepl('zand|dalgrond',soiltype.n),crop_sensitivity_p := 'class0']
+  
+  # replace missing ones with low sensitivity
+  dt[is.na(crop_sensitivity_p),crop_sensitivity_p :='class4']
+  # Determine crop classication for K ----
+  
+  # crop name selection strings for potassium on sandy soils and peat (zand, dekzand, dalgrond en veen)
+  cr2 = 'aardappelen, poot|aardappelen, zetme|aardappelen, bestr|bloemkool'
+  cr3 = 'bieten, voeder'
+  cr1 = 'broccoli|cichorei|^sla|aardappelen, consumptie|bieten, suiker|rode bieten|klaver|wikke|luzerne|uien|spinazie|spruit|wortel|peen|prei|augurk|knols|schorse|aardbei|vlas|karwij|kool'  
+  cr4 = 'raapzaad|asperges|tagetes|granen|hennep|haver|sorghum|triticale|spelt|graszoden|tarwe|rogge|grasza|gerst|mais|corncob|snijmai|maÃ¯s|bonen|boon|erwt'
+  nat = 'bos|rand|griend|natuur|houtwal|onbeteeld|onbekend|struweel|oever|rietzoom|wandel|wilg|brandn|cultuur|poel|bomenrij'
+  bkw = 'bomen|peren|bloem|rozen|facelia|hop|lupine|kers|bramen|heesters|elzen|pruimen|zonne|conifer|kruid|vaste|heg|boom|wijn|bloemkwek|heesters|appelen|buxus|bes'
+  
+  # distinghuish K sensitivity crop classes for grassland, nature and tree & bulbs
+  dt[grepl(grs,crop_name), crop_sensitivity_k := 'gras']
+  dt[grepl(nat,crop_name), crop_sensitivity_k := 'natuur'] 
+  dt[grepl(bkw,crop_name), crop_sensitivity_k := 'bkw']
+  
+  # distinghuish K sensitivity arable crop classes on sandy soils
+  soilsel <- 'zand|dekzand|dalgrond|veen'
+  dt[grepl(cr1,crop_name) & grepl(soilsel,soiltype.n),crop_sensitivity_k := 'class1']
+  dt[grepl(cr2,crop_name) & grepl(soilsel,soiltype.n),crop_sensitivity_k := 'class2']
+  dt[grepl(cr3,crop_name) & grepl(soilsel,soiltype.n),crop_sensitivity_k := 'class3']
+  dt[grepl(cr4,crop_name) & grepl(soilsel,soiltype.n),crop_sensitivity_k := 'class4']
+  
+  # crop name selection strings for potassium on loess soils
+  cr2 = 'aardappelen, poot|aardappelen, zetme|aardappelen, bestr|bloemkool|erwt|boon|bonen|luzerne|witlof|spruitkool' 
+  cr3 = 'raapzaad|blauwmaan|asperges|tagetes|granen|hennep|haver|sorghum|triticale|spelt|graszoden|tarwe|rogge|grasza|gerst|mais|corncob|snijmai|maÃ¯s|zaad'
+  cr1 = 'broccoli|bol|cichorei|^sla|bieten, voeder|aardappelen, consumptie|bieten, suiker|rode bieten|klaver|wikke|luzerne|uien|spinazie|spruit|wortel|peen|prei|augurk|knols|schorse|aardbei|vlas|karwij|kool'  
+  
+  # distinghuish K sensitivity arable crop classes on loess soils
+  dt[grepl(cr1,crop_name) & soiltype == 'loess',crop_sensitivity_k := 'class1']
+  dt[grepl(cr2,crop_name) & soiltype == 'loess',crop_sensitivity_k := 'class2']
+  dt[grepl(cr3,crop_name) & soiltype == 'loess',crop_sensitivity_k := 'class3']
+  
+  # crop name selection strings for potassium on clay soils
+  cr2 = 'bieten, suiker|rode bieten|^vlas|vezelvlas|karwij|asperge'
+  cr5 = 'spinazie, productie'
+  cr4 = 'raapzaad|blauwmaan|tagetes|granen|hennep|haver|sorghum|triticale|spelt|graszoden|tarwe|rogge|grasza|gerst|mais|corncob|snijmai|maÃ¯s|raapzaad|graszaad|koolzaad|kanarie|lijnzaad'
+  cr3 = 'luzerne|aardappelen, poot|aardappelen, zetme|aardappelen, bestr|bieten, voeder|peters|erwt|bonen|boon|klaver|wikke|luzene|witlof|bloemkool|spruitkool|bol'
+  cr1 = 'witte kool|rodekool|aardappelen, consumptie|^ui|uien|peen|prei|knolsel|augurk|schorsen|aardbei'
+  
+  # distinghuish K sensitivity arable crop classes on clay soils
+  dt[grepl(cr1,crop_name) & grepl('klei',soiltype.n), crop_sensitivity_k := 'class1']
+  dt[grepl(cr2,crop_name) & grepl('klei',soiltype.n), crop_sensitivity_k := 'class2']
+  dt[grepl(cr3,crop_name) & grepl('klei',soiltype.n), crop_sensitivity_k := 'class3']
+  dt[grepl(cr4,crop_name) & grepl('klei',soiltype.n), crop_sensitivity_k := 'class4']
+  dt[grepl(cr5,crop_name) & grepl('klei',soiltype.n), crop_sensitivity_k := 'class5']
+  
+  # replace missing ones with low sensitivity
+  dt[is.na(crop_sensitivity_k), crop_sensitivity_k := 'class4']
+  
+  # Determine crop classication for S ----
+  
+  # crop name selection strings for sulphur
+  cr1 ='spruitkool|sluitkool'
+  cr2 = 'bloemkool|chinese kool|knolsel|koolz'
+  cr4 = 'aardappelen, poot|^sla|bieten, suiker|vlas'
+  cr3 = 'peen|aardappelen, zetme|aardappelen, bestr|aardappelen, consumptie|boerenkool|broccoli|granen|hennep|haver|sorghum|triticale|spelt|graszoden|tarwe|rogge|grasza|gerst|mais|corncob|snijmai|maÃ¯s|prei|uien|^ui|zaaiui|erwt|boon|bonen'
+  
+  # distinghuish K sensitivity crop classes for grassland, nature and tree & bulbs
+  dt[grepl(grs,crop_name), crop_sensitivity_s := 'gras']
+  dt[grepl(nat,crop_name), crop_sensitivity_s := 'natuur'] 
+  dt[grepl(bkw,crop_name), crop_sensitivity_s := 'bkw']
+  
+  # distinghuish S sensitivity arable crop classes 
+  dt[grepl(cr1,crop_name), crop_sensitivity_s := 'class1']
+  dt[grepl(cr2,crop_name), crop_sensitivity_s := 'class2']
+  dt[grepl(cr3,crop_name), crop_sensitivity_s := 'class3']
+  dt[grepl(cr4,crop_name), crop_sensitivity_s := 'class4']
+  
+  # replace missing ones with low sensitivity
+  dt[is.na(crop_sensitivity_s), crop_sensitivity_s := 'class4']
+  
+  # return crop database
+  return(dt)  
+}
+
