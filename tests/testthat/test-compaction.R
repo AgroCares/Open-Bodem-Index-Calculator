08f4dc2915620bbf8f707f377ca61981b229b974
Sven <37927107+SvenVw@users.noreply.github.com>
2019-07-31 12:58:01 +0300

    Fixed test

--- tests/testthat/test-compaction.R
+++ tests/testthat/test-compaction.R
@@ -1,16 +1,18 @@
 test_that("ind_compaction works", {
   expect_equal(
     ind_compaction(
-      B_OV_WENR = c('bebouwing','water','glastuinbouw','zeer beperkt','beperkt','matig','groot','zeer groot','van nature verdicht')
+      B_OV_WENR = c('Zeer beperkt')
     ),
-    expected = c(1,1,1,1,0.8,0.6,0.4,0.2,0.2),
+    expected = 1,
     tolerance = 0.001
   )
   expect_equal(
     ind_compaction(
-      B_OV_WENR = c('zeer beperkt')
+      B_OV_WENR = c("Bebouwing en infrastructuur","Groot","Zeer groot","Matig","Water",
+                    "Glastuinbouw, niet beoordeeld","Beperkt door veenlagen","Van nature dicht" ,
+                    "Beperkt", "Zeer beperkt")
     ),
-    expected = 1,
+    expected = c(1, 0.4, 0.2, 0.6, 1, 1, 0.8, 0.2, 0.8, 1),
     tolerance = 0.001
   )
 })
