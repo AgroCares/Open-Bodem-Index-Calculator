747067001cd8a2e377a53ba96775ba7d6c14ed13
Gerard <gerard.ros@nmi-agro.nl>
2019-07-31 16:14:17 +0200

    add calcK and indK for K availability

--- R/potassium.R
+++ R/potassium.R
@@ -2,18 +2,23 @@
 #' 
 #' This function calculates the K availability of a soil.
 #' 
-#' @param A_K_CC (numeric) The K-CaCl2 content of the soil in mg K / kg
-#' @param A_CEC_CO (numeric) The CEC content of the soil (mmol+ per kg)
+#' @param A_PH_CC (numeric) The acidity of the soil, measured in 0.01M CaCl2 (-)
+#' @param A_OS_GV (numeric) The organic matter content of the soil (in procent)
+#' @param A_CEC_CO (numeric) The cation exchange capacity of the soil (mmol+ per kg), analysed via Cobalt-hexamine extraction
+#' @param A_K_CC (numeric) The plant available potassium, extracted with 0.01M CaCl2 (mg per kg), 
+#' @param A_K_CEC (numeric) The occupation of the CEC with potassium (in procent)
+#' @param A_CLAY_MI (numeric) The clay content of the soil (in procent)
+#' @param B_BT_AK (character) The type of soil
 #' @param B_LU_BRP (numeric) The crop code (gewascode) from the BRP
 #' 
 #' @import data.table
 #' 
 #' @export
-calc_k_availability <- function(A_K_CC, A_K_CEC,A_CEC_CO, A_PH_CC, A_OS_GV, A_CLAY_MI, 
-                                B_LU_BRP, B_BT_AK) {
+calc_potassium_availability <- function(A_K_CC, A_K_CEC,A_CEC_CO, A_PH_CC, A_OS_GV, A_CLAY_MI, 
+                                        B_LU_BRP, B_BT_AK) {
   
   id = crop_category = soiltype.n = crop_code = soiltype = NULL
-  b = cF = kindex1 = kindex2 = A_PH_KCL = NULL
+  b = cF = kindex1 = kindex2 = A_PH_KCL = A_K_CO = NULL
   
   # Load in the datasets
   crops.obic <- as.data.table(OBIC::crops.obic)
@@ -46,7 +51,7 @@ calc_k_availability <- function(A_K_CC, A_K_CEC,A_CEC_CO, A_PH_CC, A_OS_GV, A_CL
     A_CLAY_MI = A_CLAY_MI,
     B_LU_BRP = B_LU_BRP,
     B_BT_AK = B_BT_AK,
-    value := NA_real_
+    value = NA_real_
   )
   
   # merge with crop and soil classification tables
@@ -69,25 +74,40 @@ calc_k_availability <- function(A_K_CC, A_K_CEC,A_CEC_CO, A_PH_CC, A_OS_GV, A_CL
   dt.arable <- dt[crop_category == 'akkerbouw']
   
     # derive b-factor, texture dependent correction
-    dt.arable[grepl('duin|rivier|maas|klei',soiltype) & A_OS_GV <= 10 & A_CLAY_MI <= 11, b := 1.513]
-    dt.arable[grepl('duin|rivier|maas|klei',soiltype) & A_OS_GV <= 10 & A_CLAY_MI > 11, b := 0.60226 + 1.27576 /(1 + 0.09891 * A_CLAY_MI)]
-    dt.arable[grepl('zeeklei',soiltype) & A_OS_GV > 10 & A_CLAY_MI <= 5, b := 1.513]
-    dt.arable[grepl('zeeklei',soiltype) & A_OS_GV > 10 & A_CLAY_MI > 5, b := 0.60226 + 1.27576 /(1 + 0.09891 * A_CLAY_MI)]
+    dt.arable[grepl('duin|rivier|maas|klei',B_BT_AK) & A_OS_GV <= 10 & A_CLAY_MI <= 11, b := 1.513]
+    dt.arable[grepl('duin|rivier|maas|klei',B_BT_AK) & A_OS_GV <= 10 & A_CLAY_MI > 11, b := 0.60226 + 1.27576 /(1 + 0.09891 * A_CLAY_MI)]
+    dt.arable[grepl('zeeklei',B_BT_AK) & A_OS_GV > 10 & A_CLAY_MI <= 5, b := 1.513]
+    dt.arable[grepl('zeeklei',B_BT_AK) & A_OS_GV > 10 & A_CLAY_MI > 5, b := 0.60226 + 1.27576 /(1 + 0.09891 * A_CLAY_MI)]
+    dt.arable[grepl('loess',B_BT_AK) & A_CLAY_MI <= 11, b := 1.513]
+    dt.arable[grepl('loess',B_BT_AK) & A_CLAY_MI > 11, b := 1.75 - 0.04 * 2 * A_CLAY_MI + 0.00068 * (2 * A_CLAY_MI)^2 - 0.0000041 * (2 * A_CLAY_MI)^3]
     
     # pH-KCl needed (not higher than pH is 7)
     dt.arable[,A_PH_KCL := pmin(7,(A_PH_CC - 0.5262)/0.9288)]
     
     # correction factor for texture and OS (the so called F-factor)
-    dt.arable[grepl('zand|dal|veen',soiltype), cF := 20 / (10 + A_OS_GV)]
-    dt.arable[grepl('duin|rivier|maas|klei',soiltype) & A_OS_GV <= 10, cF := b /(0.15 * A_PH_KCL-0.05)]
-    dt.arable[grepl('zeeklei',soiltype) & A_OS_GV > 10, cF := b]
+    dt.arable[grepl('zand|dal|veen',B_BT_AK), cF := 20 / (10 + A_OS_GV)]
+    dt.arable[grepl('duin|rivier|maas|klei|loess',B_BT_AK) & A_OS_GV <= 10, cF := b /(0.15 * A_PH_KCL-0.05)]
+    dt.arable[grepl('zeeklei',B_BT_AK) & A_OS_GV > 10, cF := b]
     
-    # calculate K-index based on PAE
-    dt.arable[grepl('zand|dal|veen',soiltype),kindex1 := A_K_CC * cF * 0.12046]
-    dt.arable[grepl('zand|dal|veen',soiltype),kindex2 := (A_K_CEC - 0.17 * A_CEC_CO) * cF * 0.12046]
-    dt.arable[grepl('klei|loess',soiltype),kindex1 := (1.56 * A_K_CC - 17 + 0.29 * A_K_CEC) * cF * 0.12046]
-    dt.arable[grepl('klei|loess',soiltype),kindex2 := A_K_CEC * cF * 0.12046]
+    # calculate K-COHEX as mg K per kg soil
+    dt.arable[,A_K_CO := A_K_CEC * A_CEC_CO * 0.01 * 39.098]
+    
+    # calculate K-index based on CaCl2-extracble K as wel ass K-CEC
+    dt.arable[grepl('zand|dal|veen',B_BT_AK),kindex1 := A_K_CC * cF * 0.12046]
+    dt.arable[grepl('zand|dal|veen',B_BT_AK),kindex2 := (A_K_CO - 0.17 * A_CEC_CO) * cF * 0.12046]
+    dt.arable[grepl('klei',B_BT_AK),kindex1 := (1.56 * A_K_CC - 17 + 0.29 * A_K_CO) * cF * 0.12046]
+    dt.arable[grepl('klei',B_BT_AK),kindex2 := A_K_CO * cF * 0.12046]
+    
+    # calculate K-HCL (mg K2O/ 100 g) for loess, assuming Cohex similar to HCl extraction
+    dt.arable[grepl('loess',B_BT_AK), c('kindex1','kindex2') := A_K_CO * 1.2047 * 0.1]
+             
+    # add check for K-CEC derived Kindex: should not below zero
+    dt.arable[kindex2 < 0, kindex2 := kindex1]
+    dt.arable[kindex1 < 0, kindex1 := kindex2]
     dt.arable[,value := 0.5 * (kindex1 + kindex2)]
+    
+    # replace negative values by zero
+    dt.arable[value < 0, value := 0]
     
   # Calculate the K availability for nature
   dt.nature <- dt[crop_category == 'natuur']
@@ -101,6 +121,75 @@ calc_k_availability <- function(A_K_CC, A_K_CEC,A_CEC_CO, A_PH_CC, A_OS_GV, A_CL
   
   # return value: be aware, index is different for different land use and soil combinations
   return(value)
+  }
+
+#' Calculate the indicator for Potassium Availability
+#' 
+#' This function calculates the indicator for the the Potassium Availability of the soil by using the Mg-availability calculated by \code{\link{calc_potassium_availability}}
+#' 
+#' @param D_K (numeric) The value of K-index calculated by \code{\link{calc_magnesium_availability}}
+#' @param B_LU_BRP (numeric) The crop code (gewascode) from the BRP
+#' @param B_BT_AK (character) The type of soil
+#' @param A_OS_GV (numeric) The organic matter content of the soil (in procent)
+#'
+#' @export
+ind_potassium <- function(D_K,B_LU_BRP,B_BT_AK,A_OS_GV) {
   
+  id = crop_code = soiltype = soiltype.n = crop_category = NULL
   
+  # Load in the datasets for crop types
+  crops.obic <- as.data.table(OBIC::crops.obic)
+  setkey(crops.obic, crop_code)
+  soils.obic <- as.data.table(OBIC::soils.obic)
+  setkey(soils.obic, soiltype)
+  
+  # Check inputs
+  arg.length <- max(length(D_K),length(B_LU_BRP),length(B_BT_AK))
+  checkmate::assert_numeric(D_K, lower = 0, upper = 500, any.missing = FALSE)
+  checkmate::assert_numeric(A_OS_GV, lower = 0, upper = 100, any.missing = FALSE, len = arg.length)
+  checkmate::assert_numeric(B_LU_BRP, any.missing = FALSE, min.len = 1, len = arg.length)
+  checkmate::assert_subset(B_LU_BRP, choices = unique(crops.obic$crop_code), empty.ok = FALSE)
+  checkmate::assert_character(B_BT_AK, any.missing = FALSE, min.len = 1, len = arg.length)
+  checkmate::assert_subset(B_BT_AK, choices = unique(soils.obic$soiltype), empty.ok = FALSE)
+  
+  # make data.table to save scores
+  dt = data.table(
+    id = 1:arg.length,
+    D_K = D_K,
+    A_OS_GV = A_OS_GV,
+    B_LU_BRP = B_LU_BRP,
+    B_BT_AK = B_BT_AK,
+    value = NA_real_
+  )
+  
+  # add crop names
+  dt <- merge(dt, crops.obic[, list(crop_code, crop_category)], by.x = "B_LU_BRP", by.y = "crop_code")
+  dt <- merge(dt, soils.obic[, list(soiltype, soiltype.n)], by.x = "B_BT_AK", by.y = "soiltype")
+  
+  # Evaluate the K availability for grassland (CBGV, 2019)
+  dt.grass <- dt[crop_category == 'grasland']
+  dt.grass[, value := evaluate_logistic(D_K, b = 8, x0 = 2.5, v = 8)]
+  
+  # Evaluate the K availability for maize (CBGV, 2019)
+  dt.maize <- dt[crop_category == 'mais']
+  dt.maize[, value := evaluate_logistic(D_K, b = 25, x0 = 0.9, v = 8)]
+  
+  # Evaluate the K availability for arable crops (Ros & Bussink, 2011)
+  dt.arable <- dt[crop_category == 'akkerbouw']
+  dt.arable[grepl('zand|dal|veen',B_BT_AK), value := evaluate_logistic(D_K, b = 0.3, x0 = 9, v = 1.1)]
+  dt.arable[grepl('klei',B_BT_AK) & A_OS_GV <= 10, value := evaluate_logistic(D_K, b = 0.5, x0 = 11.5, v = 1.1)]
+  dt.arable[grepl('klei',B_BT_AK) & A_OS_GV > 10, value := evaluate_logistic(D_K, b = 0.4, x0 = 11.5, v = 1.1)]
+  dt.arable[grepl('loess',B_BT_AK), value := evaluate_logistic(D_K, b = 0.5, x0 = 11.5, v = 1.1)]
+  
+  # Evaluate the K availability for nature
+  dt.nature <- dt[crop_category == 'natuur']
+  dt.nature[, value := 1]
+  
+  # Combine both tables and extract values
+  dt <- rbindlist(list(dt.grass, dt.arable,dt.maize,dt.nature), fill = TRUE)
+  setorder(dt, id)
+  value <- dt[, value]
+  
+  # return output
+  return(value)
 }
---
+++ tests/testthat/test-potassium.R
@@ -0,0 +1,172 @@
+test_that("calc_potassium_availability works", {
+  expect_equal(
+    # aardappel op dekzand
+    calc_potassium_availability(
+      A_PH_CC = 5.6,
+      A_OS_GV = 7.9,
+      A_CEC_CO = 122, 
+      A_K_CC = 95,
+      A_K_CEC = 1.56,
+      A_CLAY_MI = 1.73,
+      B_BT_AK = 'dekzand',
+      B_LU_BRP = 3732
+    ),
+    expected = 10.0,
+    tolerance = 0.1
+  )
+  expect_equal(
+    # aardappel op dekzand, zeeklei, rivierklei en loss
+    calc_potassium_availability(
+      A_PH_CC = c(5.6,7.04,5.65,6.22),
+      A_OS_GV = c(7.9,4.2,6.12,3.57),
+      A_CEC_CO = c(122,191,157,142), 
+      A_K_CC = c(95,107,73,89),
+      A_K_CEC = c(1.6,2.9,3.1,2.4),
+      A_CLAY_MI = c(1.7,26,31,14),
+      B_BT_AK = c('dekzand','zeeklei','rivierklei','loess'),
+      B_LU_BRP = rep(3732,4)
+    ),
+    expected = c(10.13,28.53,24.29,16.05),
+    tolerance = 1
+  )
+  expect_equal(
+    # grassland op dekzand, zeeklei, rivierklei en loss
+    calc_potassium_availability(
+      A_PH_CC = c(5.6,7.04,5.65,6.22),
+      A_OS_GV = c(7.9,4.2,6.12,3.57),
+      A_CEC_CO = c(122,191,157,142), 
+      A_K_CC = c(95,107,73,89),
+      A_K_CEC = c(1.6,2.9,3.1,2.4),
+      A_CLAY_MI = c(1.7,26,31,14),
+      B_BT_AK = c('dekzand','zeeklei','rivierklei','loess'),
+      B_LU_BRP = rep(265,4)
+    ),
+    expected = c(3.31,3.21,2.50,3.06),
+    tolerance = 0.01
+  )
+  expect_equal(
+    # grassland op rivierklei
+    calc_potassium_availability(
+      A_PH_CC = rep(5.2,10),
+      A_OS_GV = rep(4.6,10),
+      A_CEC_CO = seq(10,400,length.out = 10), 
+      A_K_CC = seq(10,400,length.out = 10), 
+      A_K_CEC = seq(1,8,length.out = 10), 
+      A_CLAY_MI = rep(8,10),
+      B_BT_AK = rep('rivierklei',10),
+      B_LU_BRP = rep(265,10)
+    ),
+    expected = c(1.58,2.71,3.47,3.77,3.89,3.94,3.97,3.98,3.99,3.99),
+    tolerance = 0.01
+  )
+  expect_equal(
+    # aardappel op rivierklei
+    calc_potassium_availability(
+      A_PH_CC = rep(5.2,10),
+      A_OS_GV = rep(4.6,10),
+      A_CEC_CO = seq(10,400,length.out = 10), 
+      A_K_CC = seq(10,225,length.out = 10), 
+      A_K_CEC = seq(1,8,length.out = 10), 
+      A_CLAY_MI = rep(8,10),
+      B_BT_AK = rep('rivierklei',10),
+      B_LU_BRP = rep(3732,10)
+    ),
+    expected = c(1.01,10.82,25.56,44.71,68.24,96.17,128.5,165.23,206.34,251.86),
+    tolerance = 0.01
+  )
+})
+
+test_that("ind_potassium works", {
+  expect_equal(
+    # aardappel op dekzand
+    ind_potassium(
+      D_K = 10,
+      B_BT_AK = 'dekzand',
+      A_OS_GV = 7.9,
+      B_LU_BRP = 3732
+    ),
+    expected = 0.60,
+    tolerance = 0.1
+  )
+  expect_equal(
+    # aardappel op dekzand, zeeklei, rivierklei en loss
+    ind_potassium(
+      D_K = c(10.13,28.53,24.29,16.05),
+      A_OS_GV = c(7.9,4.2,6.12,3.57),
+      B_BT_AK = c('dekzand','zeeklei','rivierklei','loess'),
+      B_LU_BRP = rep(3732,4)
+    ),
+    expected = c(0.61,0.999,0.998,0.915),
+    tolerance = 0.01
+  )
+  expect_equal(
+    # grassland op dekzand, zeeklei, rivierklei en loss
+    ind_potassium(
+      D_K = c(3.31,3.21,2.50,3.06),
+      A_OS_GV = c(7.9,4.2,6.12,3.57),
+      B_BT_AK = c('dekzand','zeeklei','rivierklei','loess'),
+      B_LU_BRP = rep(265,4)
+    ),
+    expected = c(0.99,0.99,0.917,0.998),
+    tolerance = 0.01
+  )
+  expect_equal(
+    # grassland op rivierklei
+    ind_potassium(
+      D_K = c(1.58,2.71,3.47,3.77,3.89,3.94,3.97,3.98,3.99,3.99),
+      A_OS_GV = rep(4.6,10),
+      B_BT_AK = rep('rivierklei',10),
+      B_LU_BRP = rep(265,10)
+    ),
+    expected = c(0.40,0.98,rep(0.99,8)),
+    tolerance = 0.01
+  )
+  expect_equal(
+    # aardappel op rivierklei
+    ind_potassium(
+      D_K = c(1.01,10.82,25.56,44.71,68.24,96.17,128.5,165.23,206.34,251.86),
+      A_OS_GV = rep(4.6,10),
+      B_BT_AK = rep('rivierklei',10),
+      B_LU_BRP = rep(3732,10)
+    ),
+    expected = c(0.0085,0.450,0.99,rep(1,7)),
+    tolerance = 0.1
+  )
+  
+  expect_equal(
+    # aardappel op loss
+    ind_potassium(
+      D_K = seq(1,30,length.out = 10),
+      A_OS_GV = rep(4.6,10),
+      B_BT_AK = rep('loess',10),
+      B_LU_BRP = rep(3732,10)
+    ),
+    expected = c(00.008,0.04,0.14,0.43,0.79,0.95,0.99,0.998,1,1),
+    tolerance = 0.1
+  )
+  
+  expect_equal(
+    # aardappel op zeeklei > 10%
+    ind_potassium(
+      D_K = seq(1,40,length.out = 10),
+      A_OS_GV = rep(15,10),
+      B_BT_AK = rep('zeeklei',10),
+      B_LU_BRP = rep(3732,10)
+    ),
+    expected = c(0.02,0.0986,0.36,0.75,0.94,0.99,0.99,0.99,1,1),
+    tolerance = 0.1
+  )
+  expect_equal(
+    # aardappel op dekzand
+    ind_potassium(
+      D_K = seq(1,30,length.out = 10),
+      A_OS_GV = rep(15,10),
+      B_BT_AK = rep('dekzand',10),
+      B_LU_BRP = rep(3732,10)
+    ),
+    expected = c(0.1,0.22,0.42,0.65,0.83,0.93,0.97,0.99,1,1),
+    tolerance = 0.1
+  )
+  
+})
+
