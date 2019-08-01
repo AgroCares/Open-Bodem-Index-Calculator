4d628fc7df87c4931458adb18e9b687a4db683d2
Gerard <gerard.ros@nmi-agro.nl>
2019-08-01 18:56:48 +0200

    functions calcCEC and indCEC

--- R/cec.R
+++ R/cec.R
@@ -1,63 +1,50 @@
-#' Calculate the index for the CEC
+#' Calculate the index for the CEC-occupation
 #' 
-#' This function calculates the NLV (nitrogen producing capacity) for the soil
+#' This function calculates the CEC-buffer index for a soil
 #' 
-#' @param B_LU_BRP (numeric) The crop code (gewascode) from the BRP
-#' @param B_BT_AK (character) The type of soil
-#' 
+#' @param A_CEC_CO (numeric) The cation exchange capacity (mmol+ / kg) 
+#' @param A_K_CEC (numeric) The occupation of the CEC with K (\%)
+#' @param A_CA_CEC (numeric) The occupation of the CEC with Mg (\%)
+#' @param A_MG_CEC (numeric) The occupation of the CEC with Ca (\%)
+#' @param advice (character) Optional parameter to select CEC index for soil structure or fertility. Options: fertility_index or structure_index
+#'    
 #' @import data.table
 #' 
 #' @export
-calc_cec <- function(A_N_TOT, D_OC, B_LU_BRP, B_BT_AK, D_BDS, A_CN_RAT, D_GA) {
+calc_cec <- function(A_CEC_CO,A_K_CEC,A_CA_CEC,A_MG_CEC, advice ='fertility_index') {
   
-  a = c.ass = c.diss = id = crop_code = soiltype = soiltype.n = crop_n = NULL
+  id = fertility_index = structure_index = NULL
   
-  # Load in the datasets
-  crops.obic <- as.data.table(OBIC::crops.obic)
-  setkey(crops.obic, crop_code)
-  soils.obic <- as.data.table(OBIC::soils.obic)
-  setkey(soils.obic, soiltype)
-  
-  # Check input
-  arg.length <- max(length(A_N_TOT), length(D_OC), length(B_LU_BRP), length(B_BT_AK), length(D_BDS), length(D_GA))
-  checkmate::assert_numeric(A_N_TOT, lower = 0, upper = 30000, any.missing = FALSE, len = arg.length)
-  checkmate::assert_numeric(D_OC, lower = 0, any.missing = FALSE, len = arg.length)
-  checkmate::assert_numeric(B_LU_BRP, any.missing = FALSE, min.len = 1, len = arg.length)
-  checkmate::assert_subset(B_LU_BRP, choices = unique(crops.obic$crop_code), empty.ok = FALSE)
-  checkmate::assert_character(B_BT_AK, any.missing = FALSE, min.len = 1, len = arg.length)
-  checkmate::assert_subset(B_BT_AK, choices = unique(soils.obic$soiltype), empty.ok = FALSE)
-  checkmate::assert_numeric(D_BDS, lower = 0, upper = 1500, any.missing = FALSE, len = arg.length)
-  checkmate::assert_numeric(A_CN_RAT, lower = 0, upper = 50, any.missing = FALSE, len = arg.length)
-  checkmate::assert_numeric(D_GA, lower = 0, upper = 99, len = arg.length)
-  
-  # Settings
-  param.a <- 20 # Age of organic matter
-  param.b <- 2^((14.1 - 9)/ 9) # Temperature correction
-  param.cn.micro <- 10 # CN ratio of micro organisms
-  param.t <- 5 / 12 # 5 months a year
-  param.diss.micro <- 2 # Dissimilation : assimilation ratio of micro organisms
+  # Check inputs
+  arg.length <- max(length(A_CEC_CO), length(A_K_CEC), length(A_CA_CEC), length(A_MG_CEC))
+  checkmate::assert_numeric(A_CEC_CO, lower = 0, upper = 1000, any.missing = FALSE, len = arg.length)
+  checkmate::assert_numeric(A_K_CEC, lower = 0, upper = 20, any.missing = FALSE, len = arg.length)
+  checkmate::assert_numeric(A_CA_CEC, lower = 0, upper = 100, any.missing = FALSE, len = arg.length)
+  checkmate::assert_numeric(A_MG_CEC, lower = 0, upper = 20, any.missing = FALSE, len = arg.length)
+  checkmate::assert_character(advice, any.missing = FALSE, min.len = 1, len = 1)
+  checkmate::assert_subset(advice, choices = c('fertility_index','structure_index'), empty.ok = FALSE)
   
   # Collect data in a table
   dt <- data.table(
     id = 1:arg.length,
-    A_N_TOT = A_N_TOT,
-    D_OC = D_OC,
-    B_LU_BRP = B_LU_BRP,
-    B_BT_AK = B_BT_AK,
-    D_BDS = D_BDS,
-    A_CN_RAT = A_CN_RAT,
-    D_GA = D_GA,
-    value = NA_real_
-  )
-  dt <- merge(dt, crops.obic[, list(crop_code, crop_n)], by.x = "B_LU_BRP", by.y = "crop_code")
-  dt <- merge(dt, soils.obic[, list(soiltype, soiltype.n)], by.x = "B_BT_AK", by.y = "soiltype")
+    A_CEC_CO = A_CEC_CO,
+    A_K_CEC = A_K_CEC,
+    A_CA_CEC = A_CA_CEC,
+    A_MG_CEC = A_MG_CEC,
+    fertility_index = NA_real_,
+    structure_index = NA_real_
+    )
   
-
-  # Combine both tables and extract values
-  dt <- rbindlist(list(dt.grass, dt.arable), fill = TRUE)
+  # Calculate CEC index for soil fertility
+  dt[, fertility_index := A_CEC_CO]
+  
+  # Calculate CEC index for structure (normalized to value beween 0-1)
+  dt[, structure_index := sqrt((A_CA_CEC - 80)^2 + (A_MG_CEC - 8.5)^2 + (A_K_CEC - 14.5)^2) / 125]
+  
+  # select output value given required advice
   setorder(dt, id)
-  value <- dt[, value]
-  
+  dt[, value := mget(advice)]
+  value <- dt[,value]
   return(value)
 }
 
@@ -66,15 +53,20 @@ calc_cec <- function(A_N_TOT, D_OC, B_LU_BRP, B_BT_AK, D_BDS, A_CN_RAT, D_GA) {
 #' This function calculates the indicator for the the CEC  of the soil by using the CEC-index calculated by \code{\link{calc_cec}}
 #' 
 #' @param D_CEC (numeric) The value of NLV  calculated by \code{\link{calc_cec}}
+#' @param advice (character) Optional parameter to select CEC index for soil structure or fertility. Options: fertility_index or structure_index
 #' 
 #' @export
-ind_cec <- function(D_CEC) {
+ind_cec <- function(D_CEC,advice = 'fertility_index') {
   
   # Check inputs
-  checkmate::assert_numeric(D_CEC, lower = -30, upper = 250, any.missing = FALSE)
+  if(advice == 'fertility_index'){checkmate::assert_numeric(D_CEC, lower = 0, upper = 1000, any.missing = FALSE)} 
+  if(advice == 'structure_index'){checkmate::assert_numeric(D_CEC, lower = 0, upper =  5, any.missing = FALSE)} 
   
-  # Evaluate the nitrogen
-  value <- OBIC::evaluate_parabolic(D_CEC, x.top = 120)
+  # Evaluate the CEC for agricultural production, given impact soil fertility (Goselink & Van Erp, 1999)
+  if(advice == 'fertility_index'){value <- pmin(D_CEC * 0.01, 1)}
+  
+  # Evaluate the CEC for soil structure aspects (as used by Eurofins)
+  if(advice == 'structure_index'){value <- 1 - D_CEC}
   
   # return output
   return(value)
