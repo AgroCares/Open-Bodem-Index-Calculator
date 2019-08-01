181a7535751fb976fcfa41eb17680ae5564ac082
Gerard <gerard.ros@nmi-agro.nl>
2019-08-02 01:17:50 +0200

    func for Cu and Zn v1 added

--- R/metals.R
+++ R/metals.R
@@ -11,7 +11,7 @@
 #' @export
 calc_copper_availability <- function(A_CU_CC, B_LU_BRP, B_BT_AK) {
   
-  id = crop_code = soiltype = soiltype.n = crop_n = NULL
+  id = crop_code = soiltype = soiltype.n = crop_n = crop_category = NULL
   
   # Load in the datasets
   crops.obic <- as.data.table(OBIC::crops.obic)
@@ -37,10 +37,14 @@ calc_copper_availability <- function(A_CU_CC, B_LU_BRP, B_BT_AK) {
     B_BT_AK = B_BT_AK,
     value = NA_real_
   )
-  dt <- merge(dt, crops.obic[, list(crop_code, crop_n)], by.x = "B_LU_BRP", by.y = "crop_code")
+  dt <- merge(dt, crops.obic[, list(crop_code, crop_category)], by.x = "B_LU_BRP", by.y = "crop_code")
   dt <- merge(dt, soils.obic[, list(soiltype, soiltype.n)], by.x = "B_BT_AK", by.y = "soiltype")
   
   # Calculate Cu-availability
+  dt[crop_category =='akkerbouw', value := 10^(0.948 + 0.188 * log10(A_CU_CC))]
+  dt[crop_category =='mais', value := 10^(0.948 + 0.188 * log10(A_CU_CC))]
+  dt[crop_category =='natuur', value := 0]
+  dt[crop_category =='grasland', value := A_CU_CC]
   
   # Extract relevant variable and return
   setorder(dt, id)
@@ -60,9 +64,9 @@ calc_copper_availability <- function(A_CU_CC, B_LU_BRP, B_BT_AK) {
 #' @import data.table
 #' 
 #' @export
-calc_zinc_availability <- function(A_ZN_CC, B_LU_BRP, B_BT_AK) {
+calc_zinc_availability <- function(A_ZN_CC, B_LU_BRP, B_BT_AK, A_PH_CC) {
   
-  id = crop_code = soiltype = soiltype.n = crop_n = NULL
+  id = crop_code = soiltype = soiltype.n = crop_n = crop_category = NULL
   
   # Load in the datasets
   crops.obic <- as.data.table(OBIC::crops.obic)
@@ -73,25 +77,29 @@ calc_zinc_availability <- function(A_ZN_CC, B_LU_BRP, B_BT_AK) {
   # Check input
   arg.length <- max(length(A_ZN_CC), length(B_LU_BRP), length(B_BT_AK))
   checkmate::assert_numeric(A_ZN_CC, lower = 0, upper = 30000, any.missing = FALSE, len = arg.length)
+  checkmate::assert_numeric(A_PH_CC, lower = 3, upper = 10, any.missing = FALSE, len = arg.length)
   checkmate::assert_numeric(B_LU_BRP, any.missing = FALSE, min.len = 1, len = arg.length)
   checkmate::assert_subset(B_LU_BRP, choices = unique(crops.obic$crop_code), empty.ok = FALSE)
   checkmate::assert_character(B_BT_AK, any.missing = FALSE, min.len = 1, len = arg.length)
   checkmate::assert_subset(B_BT_AK, choices = unique(soils.obic$soiltype), empty.ok = FALSE)
   
-  # Settings
-  
   # Collect data in a table
   dt <- data.table(
     id = 1:arg.length,
     A_ZN_CC = A_ZN_CC,
+    A_PH_CC = A_PH_CC,
     B_LU_BRP = B_LU_BRP,
     B_BT_AK = B_BT_AK,
     value = NA_real_
   )
-  dt <- merge(dt, crops.obic[, list(crop_code, crop_n)], by.x = "B_LU_BRP", by.y = "crop_code")
+  dt <- merge(dt, crops.obic[, list(crop_code, crop_category)], by.x = "B_LU_BRP", by.y = "crop_code")
   dt <- merge(dt, soils.obic[, list(soiltype, soiltype.n)], by.x = "B_BT_AK", by.y = "soiltype")
   
-  # Calculate Zn-availability
+  # Calculate Cu-availability
+  dt[crop_category =='akkerbouw', value := 10^(0.88 + 0.56 * log10(A_ZN_CC) - 0.13 * A_PH_CC)]
+  dt[crop_category =='mais', value := 10^(0.88 + 0.56 * log10(A_ZN_CC) - 0.13 * A_PH_CC)]
+  dt[crop_category =='natuur', value := 0]
+  dt[crop_category =='grasland', value := 10^(-1.04 + 0.67 * log10(A_ZN_CC) - 0.5 * A_PH_CC)]
   
   # Extract relevant variable and return
   setorder(dt, id)
@@ -100,7 +108,6 @@ calc_zinc_availability <- function(A_ZN_CC, B_LU_BRP, B_BT_AK) {
   return(value)
 }
 
-
 #' Calculate the indicator for Cu-availability
 #' 
 #' This function calculates the indicator for the the Cu availability in soil by using the Cu-index as calculated by \code{\link{calc_copper_availability}}
@@ -108,13 +115,13 @@ calc_zinc_availability <- function(A_ZN_CC, B_LU_BRP, B_BT_AK) {
 #' @param D_CU (numeric) The value of Cu-index  calculated by \code{\link{calc_copper_availability}}
 #' 
 #' @export
-ind_nitrogen <- function(D_CU) {
+ind_copper <- function(D_CU) {
   
   # Check inputs
-  checkmate::assert_numeric(D_CU, lower = -30, upper = 250, any.missing = FALSE)
+  checkmate::assert_numeric(D_CU, lower = 0, upper = 50, any.missing = FALSE)
   
   # Evaluate the nitrogen
-  value <- OBIC::evaluate_parabolic(D_CU, x.top = 120)
+  value <- OBIC::evaluate_parabolic(D_CU, x.top = 10)
   
   # return output
   return(value)
@@ -125,16 +132,16 @@ ind_nitrogen <- function(D_CU) {
 #' 
 #' This function calculates the indicator for the the Zn availability in soil by using the Zn-index as calculated by \code{\link{calc_zinc_availability}}
 #' 
-#' @param D_ZN (numeric) The value of Cu-index  calculated by \code{\link{calc_zinc_availability}}
+#' @param D_ZN (numeric) The value of Zn-index  calculated by \code{\link{calc_zinc_availability}}
 #' 
 #' @export
-ind_nitrogen <- function(D_ZN) {
+ind_zinc <- function(D_ZN) {
   
   # Check inputs
   checkmate::assert_numeric(D_ZN, lower = -30, upper = 250, any.missing = FALSE)
   
   # Evaluate the nitrogen
-  value <- OBIC::evaluate_parabolic(D_ZN, x.top = 120)
+  value <- OBIC::evaluate_parabolic(D_ZN, x.top = 150)
   
   # return output
   return(value)
