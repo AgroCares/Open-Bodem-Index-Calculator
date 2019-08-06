74a73f0dd44dfc3d00434c1a4e439d8e38c5940b
Sven <37927107+SvenVw@users.noreply.github.com>
2019-08-06 11:37:13 +0200

    Add random recommendation

--- R/obic_recommendations.R
+++ R/obic_recommendations.R
@@ -15,21 +15,21 @@ obic_recommendations <- function(dt.score) {
   RM_C_1 = RM_C_2 = RM_C_3 = RM_P_1 = RM_P_2 = RM_P_3 = RM_B_1 = RM_B_2 = RM_B_3 = NULL
 
   # Chemical recommondations ------------------------------------------------
-  dt.score[, RM_C_1 := -999]
-  dt.score[, RM_C_2 := -999]
-  dt.score[, RM_C_3 := -999]
+  dt.score[, RM_C_1 := sample(1:21, size = .N, replace = TRUE)]
+  dt.score[, RM_C_2 := sample(1:21, size = .N, replace = TRUE)]
+  dt.score[, RM_C_3 := sample(1:21, size = .N, replace = TRUE)]
   
 
   # Physical recommendations ------------------------------------------------
-  dt.score[, RM_P_1 := -999]
-  dt.score[, RM_P_2 := -999]
-  dt.score[, RM_P_3 := -999]
+  dt.score[, RM_P_1 := sample(1:21, size = .N, replace = TRUE)]
+  dt.score[, RM_P_2 := sample(1:21, size = .N, replace = TRUE)]
+  dt.score[, RM_P_3 := sample(1:21, size = .N, replace = TRUE)]
   
 
   # Biological recommondations ----------------------------------------------
-  dt.score[, RM_B_1 := -999]
-  dt.score[, RM_B_2 := -999]
-  dt.score[, RM_B_3 := -999]
+  dt.score[, RM_B_1 := sample(1:21, size = .N, replace = TRUE)]
+  dt.score[, RM_B_2 := sample(1:21, size = .N, replace = TRUE)]
+  dt.score[, RM_B_3 := sample(1:21, size = .N, replace = TRUE)]
   
   return(dt.score)
   
