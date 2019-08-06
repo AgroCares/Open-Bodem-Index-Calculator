94b20be09d231692d2f593cb5daf2e9649b6206c
Sven <37927107+SvenVw@users.noreply.github.com>
2019-08-06 12:48:52 +0200

    typo

--- R/obic.R
+++ R/obic.R
@@ -19,7 +19,7 @@ obic <- function(dt) {
         "A_CU_CC","A_MN_CC",'A_ZN_CC',
         "A_MG_CEC", "A_MG_CC", "A_N_TOT", "A_OS_GV", "A_P_PAE", "A_P_PAL","A_PH_CC", "A_P_WA", "A_SILT_MI", "A_S_TOT",
         "A_SAND_MI", "B_GT", "B_BT_AK", "A_C_FR", "A_MB_PFLA", "A_SB_FR", "A_B_BC", "A_VO_BC", "A_RW_BC", "A_BS_BC",    
-        "A_GV_BC", "A_PV_BC", "A_AS_BC", "A_SV_BC", "A_RD_BC"," A_SS_BC", "A_CO_BC", 'A_CU_CC','A_MN_CC','A_ZN_CC', 
+        "A_GV_BC", "A_PV_BC", "A_AS_BC", "A_SV_BC", "A_RD_BC", "A_SS_BC", "A_CO_BC", 'A_CU_CC','A_MN_CC','A_ZN_CC', 
         "B_LU_BRP", "B_BT_AK", "B_NR_RIVM", "B_RWT_DANK", "B_RWA_DANK", "B_OV_WENR", 
         "B_HELP_WENR", "B_LG_CBS", 
         "M_M1", "M_M2", "M_M3", "M_M4", "M_M5", "M_M6", "M_M7", "M_M8", "M_M9", "M_M10", "M_M11", "M_M12", "M_M13", "M_M14", "M_M15"
