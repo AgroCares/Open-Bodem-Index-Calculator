6a016ee48c5b367a7aa7b132dbadacb99c094138
yuki-nmi <mbakyuki@gmail.com>
2019-10-01 13:50:29 +0200

    Alternative evaluation formula for arable land

--- R/sulpher.R
+++ R/sulpher.R
@@ -246,7 +246,9 @@ ind_sulpher <- function(D_SLV,B_LU_BRP, B_BT_AK, B_LG_CBS) {
   dt.arable <- dt[crop_category == "akkerbouw"]
   if(nrow(dt.arable)>0){
   dt.arable[,sbal := calc_sbal_arable(D_SLV, B_LU_BRP, B_BT_AK, B_LG_CBS)]
-  dt.arable[,value := evaluate_parabolic(sbal, x.top = 0)]
+  #dt.arable[,value := evaluate_parabolic(sbal, x.top = 0)]
+  ## Alternative formula suggested by YF. See Factsheet Appendix II.
+  dt.arable[,value := evaluate_logistic(sbal, b = 0.5, x0 = 0, v = 1)] 
   }
   
   # Evaluate S availability for maize land -----
