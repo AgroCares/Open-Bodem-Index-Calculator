e002b8db3706b0f4f08a44f4b6c25454d5b447ef
jobdepater <job.depater@nmi-agro.nl>
2019-10-14 22:37:34 +0200

    voorbeeldje voor Yuki

--- demo/app.R
+++ demo/app.R
@@ -1,42 +1,50 @@
 
-
+# laden packages
 library(shiny)
+library(OBIC)
 library(data.table)
 
-# Define UI for application that draws a histogram
+# Gebruikersinterface (hier definieer je hoe alles er uit komt te zien)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Demo OBIC"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            sliderInput("clay", "Kleigehalte (%)", min = 0, max = 100, value = 3, step = 2),
+            sliderInput("os", "Organische stof (%)", min = 0, max = 30, value = 3, step = 0.5),
+            sliderInput("ph", "Zuurtegraad (pH)", min = 4, max = 9, value = 6, step = 0.1)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("plot")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    # bereken OBI score voor crumbleability
+    observe({
+        crumbleability <-  calc_crumbleability(input$clay, input$os, input$ph)
+        sealing <- calc_sealing_risk(input$clay, input$os)
+        
+        output$plot <- renderPlot({
+            barplot(height = c(crumbleability, sealing),
+                    col = c(1,2),
+                    legend.text = c("Verkuimelbaarheid", "Verslemping"),
+                    args.legend = list(x = "topright"))
+        })
+        
     })
+
+  
+    
+
 }
 
 # Run the application 
