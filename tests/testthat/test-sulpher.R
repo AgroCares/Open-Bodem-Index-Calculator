e6b5ccfbe1b26f4a5472e8b6eb7a6ed1989c1c4a
Sven <37927107+SvenVw@users.noreply.github.com>
2019-11-21 10:20:39 +0100

    Update test

--- tests/testthat/test-sulpher.R
+++ tests/testthat/test-sulpher.R
@@ -72,7 +72,7 @@ test_that("ind_sulpher works", {
       B_BT_AK = rep('zeeklei',10),
       B_LG_CBS = rep('IJsselmeerpolders',10)
     ),
-    expected = c(0.018,rep(1,9)),
+    expected = c(0.0408, 0.4102,rep(1,8)),
     tolerance = 0.001
   )
 })
