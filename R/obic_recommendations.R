e71b276a68af1ec810cdbf1a27232355f2d348b2
Gerard <gerard.ros@nmi-agro.nl>
2019-12-03 01:19:35 +0100

    add local db to function

--- R/obic_recommendations.R
+++ R/obic_recommendations.R
@@ -12,210 +12,28 @@ obic_evalmeasure <- function(dt.score) {
   # Check inputs
   checkmate::assert_data_table(dt.score)
   
-  
+  # set variables as NULL
   soiltype = soiltype.n = crop_waterstress = crop_maatregel = crop_code = ID = NULL
-  Ef_M_v = maatregel_nr = OBICvariable = Order = NULL
-  Dremp_B_DI = Dremp_B_SB = Dremp_B_SF = Dremp_C_CEC = Dremp_C_CU  = NULL
-  Dremp_C_K = Dremp_C_MG = Dremp_C_N = Dremp_C_P = Dremp_C_PH = Dremp_C_S = NULL
-  Dremp_C_ZN = Dremp_P_BC = Dremp_P_CEC = Dremp_P_CO = Dremp_P_CR = Dremp_P_DU = NULL
-  Dremp_P_MS = Dremp_P_SE = Dremp_P_WRI = NULL
-  Rel_C_CEC = Rel_C_CU = Rel_C_K = Rel_C_MG = NULL
-  Rel_C_N = Rel_C_P = Rel_C_PH = Rel_C_S = Rel_C_ZN = Rel_P_BC = Rel_P_CEC = Rel_P_CO = NULL
-  Rel_P_CR = Rel_P_DU = Rel_P_MS = Rel_P_SE = Rel_P_WRI = Rel_B_DI = Rel_B_SF = Rel_B_SB = NULL
-  M_C_N = M_C_P = M_C_K = M_C_MG = M_C_S = M_C_PH = M_C_CEC = M_C_CU = M_C_ZN = NULL
-  M_P_CR = M_P_SE = M_P_MS = M_P_BC = M_P_DU = M_P_CO = M_P_CEC = M_P_WRI = NULL
-  M_B_DI = M_B_SF = M_B_SB = NULL
+  OBICvariable = maatregel_nr = Dremp_S = app = app1 = app2 = sector = grondsoort = indicator = NULL
+  Ef_M_v = tresshold = score.m = weight = grp = score.mp = Prio_M = m.effect = FS = TH = NULL
   
-  ## Settings------------------------------------------------------------
-
-  # base of indicator variables
-  nm <- c( "C_N","C_P","C_K","C_MG","C_S","C_PH","C_CEC","C_CU","C_ZN",
-           "P_CR","P_SE","P_MS","P_BC","P_DU","P_CO","P_WRI","P_CEC",
-           "B_DI","B_SF","B_SB")
-  # name of indicator variables which are used to calculate integrated scores (e.g. I_C_N)
-  ind_nm <- paste0("I_", nm)
+  # make local copy of dt.score
+  dt.score <- copy(dt.score)
   
-  ## Load in the datasets ------------------------------------------------
-  # weighing factor to calculate scores
-  w <- as.data.table(OBIC::weight.obic)
-  w$rsid <- 1
-  w <- dcast(w,rsid~var,value.var = 'weight')
+  # add local databases for joining properties -----
   
-  # effects of measures
-  maatregel.obic <- as.data.table(OBIC::maatregel.obic)
-  
-  # crop categories
-  crops.obic <- as.data.table(OBIC::crops.obic)
-  
-  # soil categories
-  soils.obic <- as.data.table(OBIC::soils.obic)
-  
-  
-  # Merge soil and crop categories to dt.score ---------------------------------------
-  # Make an additional category for loess
-  soils.obic[soiltype == 'loess', soiltype.n := 'loess'] 
-  setkey(soils.obic, soiltype)
-  dt.score <- merge(dt.score, soils.obic[, list(soiltype, soiltype.n)], by.x = "B_BT_AK", by.y = "soiltype")
-  
-  # make a new column for crop category for evaluating measure effects
-  crops.obic[grepl('grasland zonder herinzaai|grasland met herinzaai|natuur', crop_waterstress), crop_maatregel := "melkveehouderij"]
-  crops.obic[grepl('granen|suikerbiet|aardappel|snijmais|overig', crop_waterstress), crop_maatregel := "akkerbouw"]
-  crops.obic[grepl('bladgroenten|wintergroenten|zomergroenten|bloembollen', crop_waterstress), crop_maatregel := "groenten"]
-  crops.obic[grepl('boomteelt|overig boomteelt|groot fruit|klein fruit', crop_waterstress), crop_maatregel := "boomteelt"]
-  setkey(crops.obic, crop_code)
-  dt.score <- merge(dt.score, crops.obic[, list(crop_code, crop_maatregel)], by.x = "B_LU_BRP", by.y = "crop_code")
-  setkey(dt.score, ID)
-  
-  # Pre-processing of effect table -----------------------------------------
-  
-  # Relative importance of 11 measures
-  Prio_M <- maatregel.obic[OBICvariable == ind_nm[1], Prio_M]
-  names(Prio_M) <- paste0("M", 1:11)
-  
-  # Order of 11 measures (this matters when 2 measures get the same score (=tie))
-  order_M <- maatregel.obic[OBICvariable == ind_nm[1], Order]
-  names(order_M) <- paste0("M", 1:11)
-  
-  # Threshold values of indicators (above which positive effects of measure is not considered)
-  # Ordered as in 'ind_nm'
-  Dremp_value <- maatregel.obic$Dremp_S[match(ind_nm, maatregel.obic[maatregel_nr == 1, OBICvariable])]
-  names(Dremp_value) <- nm
-  
-  ## Calculation of scores of measures -----------------------------------------
-  # Make a new datatable to store the effects of measures
-  dt.recom <- copy(dt.score)
-  
-  # Compute, per record, if each indicator value is below the threshold value (e.g. 'Dremp_C_N', either 1 or 0)
-  # The threshold values are indicator-specific
-  dt.recom[, (paste0("Dremp_", nm)) := 0] # initialization
-  for(j in nm){ # loop over all indicators
-    col = paste0("I_", j)
-    nr_blth <- which(dt.recom[, mget(col)] < Dremp_value[j]) # row number in which indicator value is below the threshold
-    set(dt.recom, i = nr_blth, j = paste0("Dremp_", j), value = 1)
-  }
-  
-  # Calculate, for each measure m, the scores of 11 measures
-  # in the order as in "order_M" (the order matters when there are ties in the scores)
-  for (m in gsub('M', '', names(sort(order_M)))){ 
-    # subset the effect table
-    maatregel_sel <- subset(maatregel.obic,  maatregel_nr == m) 
-    # drop the indicators which does not match the prescribed list 
-    maatregel_sel <- maatregel_sel[grepl(paste0('^', ind_nm, '$', collapse = "|"), maatregel_sel$OBICvariable), ]
+    # wegingsfactoren voor integratie per bodemfunctie type
+    w <- as.data.table(OBIC::weight.obic)
     
-    ## check if there is missing data for any indicator (and if so, fill their effect as 0)
-    # (This part can be removed after checking that 'maatregel.obic' contains all combinations of 11 measures x indicators)
-    # check if all needed indicator variables are included.
-    nmnr <- ind_nm[is.na(match(ind_nm, maatregel_sel$OBICvariable))] # name of missing indicators
-    nrna <-  length(nmnr) # number of missings indicators
-    # fill their effect as 0
-    if(nrna > 0){
-      # show message on screen
-      print(paste(nrna, "indicators are missing for measure", m, ':', paste(nmnr, collapse = ', ')))
-      print( 'Their effects are now assumed to be 0.')
-      # duplicate the first rows (initialization)
-      maatregel_sel <- rbind(maatregel_sel, maatregel_sel[1:nrna, ])
-      # fill NA first (except first 4 columns)
-      maatregel_sel[c((nrow(maatregel_sel)-nrna+1):nrow(maatregel_sel)), c(5:ncol(maatregel_sel)) := NA]
-      # fill missing indicator names
-      maatregel_sel[c((nrow(maatregel_sel)-nrna+1):nrow(maatregel_sel)), OBICvariable :=  as.character(as.list(nmnr))]
-      # fill 0 for effect
-      colnm <- c("melkveehouderij..alleen.gras.","melkveehouderij.incl.mais.naast.gras","akkerbouw","groente", "boomteelt","klei","veen", "zand","loss")
-      maatregel_sel[c((nrow(maatregel_sel)-nrna+1):nrow(maatregel_sel)), (colnm) := 0]
-      maatregel_sel[c((nrow(maatregel_sel)-nrna+1):nrow(maatregel_sel)), Ef_M_v := 0]
-    }
+    # measures database
+    maatregel.obic <- as.data.table(OBIC::maatregel.obic)
     
-    # reorder the I_ variables (as in 'int_nm')
-    maatregel_sel <- maatregel_sel[match(ind_nm, maatregel_sel$OBICvariable),]
+    # crop categories
+    crops.obic <- as.data.table(OBIC::crops.obic)
     
-    # Effect of the measure m based on crop type
-    dt.recom[, (paste0("Ec_", nm)) := NA_real_] # initialization
-    dt.recom[crop_maatregel == "melkveehouderij",
-             (paste0("Ec_", nm)) := as.list(maatregel_sel$melkveehouderij_alleen_gras)]
-    # dt.recom[crop_maatregel == "melkveehouderij" & D_CP_MAIS < 0.2,
-    #           (paste0("Ec_", nm)) := as.list(maatregel_sel$melkveehouderij_alleen_gras)]
-    # dt.recom[crop_maatregel == "melkveehouderij" & D_CP_MAIS >= 0.2,
-    #           (paste0("Ec_", nm)) := as.list(maatregel_sel$melkveehouderij_incl_mais_naast_gras)]
-    dt.recom[crop_maatregel == "akkerbouw",
-             (paste0("Ec_", nm)) := as.list(maatregel_sel$akkerbouw)]
-    dt.recom[crop_maatregel == "groenten",
-             (paste0("Ec_", nm)) := as.list(maatregel_sel$groente)]
-    dt.recom[crop_maatregel == "boomtelt",
-             (paste0("Ec_", nm)) := as.list(maatregel_sel$boomteelt)]
+    # soil categories
+    soils.obic <- as.data.table(OBIC::soils.obic)
     
-    # Effect of the measure m based on soil type
-    dt.recom[, (paste0("Es_", nm)) := NA_real_] # initialization
-    dt.recom[soiltype.n == "klei", (paste0("Es_", nm)) := as.list(maatregel_sel$klei)]
-    dt.recom[soiltype.n == "veen", (paste0("Es_", nm)) := as.list(maatregel_sel$veen)]
-    dt.recom[soiltype.n == "zand", (paste0("Es_", nm)) := as.list(maatregel_sel$zand)]
-    dt.recom[soiltype.n == "loess", (paste0("Es_", nm)) := as.list(maatregel_sel$loss)]
-    
-    # Combined effect based both on soil type and crop type (Take the smaller one)
-    dt.recom[, (paste0("Rel_", nm)) := NA_real_] # initialization
-    for(j in nm){ # loop over all indicators
-      set(dt.recom, i = NULL, j = paste0("Rel_", j), 
-          value = pmin(dt.recom[[paste0("Es_", j)]], dt.recom[[paste0("Ec_", j)]]))
-    }
-    
-    ## separate positive and negative effects of the measure m
-    # Positive effect (0 ~ +3) of the measure on each indicator, orderd as in 'nm'
-    pEF_M <- ifelse(maatregel_sel$Ef_M_v >= 0, maatregel_sel$Ef_M_v, 0)
-    names(pEF_M)<- nm
-    # negative effect (-1) of the measure on each indicator, orderd as in 'nm'
-    nEF_M <- ifelse(maatregel_sel$Ef_M_v < 0, maatregel_sel$Ef_M_v, 0)
-    names(nEF_M)<- nm
-    
-    ## Chemical indicators ##
-    # Calculate score of the measure for each chemical indicators
-    dt.recom[, M_C_N := w$W_C_N * (Rel_C_N * pEF_M["C_N"] * Dremp_C_N + Rel_C_N * nEF_M["C_N"])]
-    dt.recom[, M_C_P := w$W_C_P * (Rel_C_P * pEF_M["C_P"] * Dremp_C_P + Rel_C_P * nEF_M["C_P"])]
-    dt.recom[, M_C_K := w$W_C_K * (Rel_C_K * pEF_M["C_K"] * Dremp_C_K + Rel_C_K * nEF_M["C_K"])]
-    dt.recom[, M_C_MG := w$W_C_MG * (Rel_C_MG * pEF_M["C_MG"] * Dremp_C_MG + Rel_C_MG * nEF_M["C_MG"])]
-    dt.recom[, M_C_S := w$W_C_S * (Rel_C_S * pEF_M["C_S"] * Dremp_C_S + Rel_C_S * nEF_M["C_S"])]
-    dt.recom[, M_C_PH := w$W_C_PH * (Rel_C_PH * pEF_M["C_PH"] * Dremp_C_PH + Rel_C_PH * nEF_M["C_PH"])]
-    dt.recom[, M_C_CEC := w$W_C_CEC * (Rel_C_CEC * pEF_M["C_CEC"] * Dremp_C_CEC + Rel_C_CEC * nEF_M["C_CEC"])]
-    dt.recom[, M_C_CU := w$W_C_CU * (Rel_C_CU * pEF_M["C_CU"] * Dremp_C_CU + Rel_C_CU * nEF_M["C_CU"])]
-    dt.recom[, M_C_ZN := w$W_C_ZN * (Rel_C_ZN * pEF_M["C_ZN"] * Dremp_C_ZN + Rel_C_ZN * nEF_M["C_ZN"])]
-    
-    # Calculate score of the measure for the chemical indicators, e.g. M1_S_C
-    dt.recom[, (paste0("M", m, "_S_C")) := 	Prio_M[paste0('M', m)] * (
-      M_C_N + M_C_P + M_C_K + M_C_MG + M_C_S + M_C_PH + M_C_CEC + M_C_CU + M_C_ZN)]
-    
-    
-    ## Physical indicators ##
-    # Calculate score of the measure for each physical indicators
-    dt.recom[, M_P_CR := w$W_P_CR * (Rel_P_CR * pEF_M["P_CR"] * Dremp_P_CR + Rel_P_CR * nEF_M["P_CR"])]
-    dt.recom[, M_P_SE := w$W_P_SE * (Rel_P_SE * pEF_M["P_SE"] * Dremp_P_SE + Rel_P_SE * nEF_M["P_SE"])]
-    dt.recom[, M_P_MS := w$W_P_MS * (Rel_P_MS * pEF_M["P_MS"] * Dremp_P_MS + Rel_P_MS * nEF_M["P_MS"])] 
-    dt.recom[, M_P_BC := w$W_P_BC * (Rel_P_BC * pEF_M["P_BC"] * Dremp_P_BC + Rel_P_BC * nEF_M["P_BC"])]
-    dt.recom[, M_P_DU := w$W_P_DU * (Rel_P_DU * pEF_M["P_DU"] * Dremp_P_DU + Rel_P_DU * nEF_M["P_DU"])]
-    dt.recom[, M_P_CO := w$W_P_CO * (Rel_P_CO * pEF_M["P_CO"] * Dremp_P_CO + Rel_P_CO * nEF_M["P_CO"])] 
-    dt.recom[, M_P_CEC := w$W_P_CEC * (Rel_P_CEC * pEF_M["P_CEC"] * Dremp_P_CEC + Rel_P_CEC * nEF_M["P_CEC"])] 
-    dt.recom[, M_P_WRI := w$W_P_WRI * (Rel_P_WRI * pEF_M["P_WRI"] * Dremp_P_WRI + Rel_P_WRI * nEF_M["P_WRI"])]
-    
-    # Calculate score of the measure for the physical indicators, e.g. M1_S_P
-    dt.recom[, (paste0("M", m, "_S_P")) :=  Prio_M[paste0('M', m)] * (
-      M_P_CR + M_P_SE + M_P_MS + M_P_BC + M_P_DU + M_P_CO + M_P_CEC + M_P_WRI)]
-    
-    ## Biological indicators ##
-    # Calculate score of the measure for each biological indicators
-    #dt.recom[, (paste0("M", m, "_S_B")) := Prio_M[paste0('M', m)] * ( 
-    dt.recom[, M_B_DI :=  w$W_B_DI * (Rel_B_DI * pEF_M["B_DI"] * Dremp_B_DI + Rel_B_DI * nEF_M["B_DI"])]
-    dt.recom[, M_B_SF :=  w$W_B_SF * (Rel_B_SF * pEF_M["B_SF"] * Dremp_B_SF + Rel_B_SF * nEF_M["B_SF"])]
-    dt.recom[, M_B_SB :=  w$W_B_SB * (Rel_B_SB * pEF_M["B_SB"] * Dremp_B_SB + Rel_B_SB * nEF_M["B_SB"])]
-    
-    # Calculate score of the measure for the biological indicators, e.g. M1_S_P
-    dt.recom[, (paste0("M", m, "_S_B")) := Prio_M[paste0('M', m)] * (M_B_DI + M_B_SF + M_B_SB)]
-    
-    
-    # Save score of each indicator (by renaming the columns)
-    # This part can be skipped if one does not want to add too many columns.
-    setnames(dt.recom, 
-             names(dt.recom)[grepl("M_", names(dt.recom))], 
-             gsub("M_", paste0("M", m, "_"), names(dt.recom)[grepl("M_", names(dt.recom))]))
-    
-    # remove unnecessary columns
-    dt.recom[, (c(paste0("Rel_", nm), paste0("Es_", nm), paste0("Ec_", nm))) := NULL]
-  }
   
   return(dt.recom)
 }
@@ -233,110 +51,7 @@ obic_evalmeasure <- function(dt.score) {
 #' @export
 obic_recommendations <- function(dt.recom, extensive = FALSE) {
   
-  # Check inputs
-  checkmate::assert_data_table(dt.recom)
-  
-  first_C = second_C = third_C = first_P = second_P = third_P = first_B = second_B = third_B = NULL
-  first_C_score = second_C_score = third_C_score = first_P_score = second_P_score = third_P_score = first_B_score = second_B_score = third_B_score = NULL
-  nr_blth_C = nr_blth_P = nr_blth_B = NULL
-  RM_C_1 = RM_C_2 = RM_C_3 = RM_P_1 = RM_P_2 = RM_P_3 = RM_B_1 = RM_B_2 = RM_B_3 = NULL
-  
-  ## Chemical recommondations ------------------------------------------------  
-  # Choose best 3 measures for chemical indicators
-  # In case of ties, the one with most left (i.e. smallest 'Order' value) is taken first.
-  cols = colnames(dt.recom)[grepl("_S_C", colnames(dt.recom))]
-  dt.recom[, first_C := colnames(.SD)[apply(.SD, 1, function(x) which(rank(x, ties.method = 'last') == length(cols)))], .SDcols = cols]
-  dt.recom[, second_C := colnames(.SD)[apply(.SD, 1, function(x) which(rank(x, ties.method = 'last') == length(cols) - 1))], .SDcols = cols]
-  dt.recom[, third_C := colnames(.SD)[apply(.SD, 1, function(x) which(rank(x, ties.method = 'last') == length(cols) - 2))], .SDcols = cols]
-  
-  # Store scores of the 1st, 2nd, and 3rd measures
-  dt.recom[, first_C_score := dt.recom[[first_C]][.I], by=first_C]
-  dt.recom[, second_C_score := dt.recom[[second_C]][.I], by=second_C]
-  dt.recom[, third_C_score := dt.recom[[third_C]][.I], by=third_C]
-  
-  # Get measure names to recommend (e.g. "M3")
-  dt.recom[, RM_C_1 := gsub("_S_C", "", first_C)]
-  dt.recom[, RM_C_2 := gsub("_S_C", "", second_C)]
-  dt.recom[, RM_C_3 := gsub("_S_C", "", third_C)]
-  
-  ## when the score of the selected measure is <=0, discard the advice 
-  dt.recom[first_C_score <= 0, RM_C_1 := "no suitable advice"]
-  dt.recom[second_C_score <= 0, RM_C_2 := "no suitable advice"]
-  dt.recom[third_C_score <= 0, RM_C_3 := "no suitable advice"]
-  
-  ## when no indicator is below the threshold level, give no advice.
-  # count number of indicators which are below threshold level
-  cols = colnames(dt.recom)[grepl("Dremp_C_", colnames(dt.recom))]
-  dt.recom[, nr_blth_C := rowSums(.SD), .SDcols = cols]
-  dt.recom[nr_blth_C == 0, c("RM_C_1", "RM_C_2", "RM_C_3")  := "no advice needed"]
-  
-  
-  ## Physical recommendations ------------------------------------------------
-  # Choose best 3 measures for physical indicators
-  # In case of ties, the one with most left (i.e. smallest 'Order' value) is taken first.
-  cols = colnames(dt.recom)[grepl("_S_P", colnames(dt.recom))]
-  dt.recom[, first_P := colnames(.SD)[apply(.SD, 1, function(x) which(rank(x, ties.method = 'last') == length(cols)))], .SDcols = cols]
-  dt.recom[, second_P := colnames(.SD)[apply(.SD, 1, function(x) which(rank(x, ties.method = 'last') == length(cols) - 1))], .SDcols = cols]
-  dt.recom[, third_P := colnames(.SD)[apply(.SD, 1, function(x) which(rank(x, ties.method = 'last') == length(cols) - 2))], .SDcols = cols]
-  
-  # Store scores of the 1st, 2nd, and 3rd measures
-  dt.recom[, first_P_score := dt.recom[[first_P]][.I], by=first_P]
-  dt.recom[, second_P_score := dt.recom[[second_P]][.I], by=second_P]
-  dt.recom[, third_P_score := dt.recom[[third_P]][.I], by=third_P]
-  
-  # Get measure names to recommend (e.g. "M3")
-  dt.recom[, RM_P_1 := gsub("_S_P", "", first_P)]
-  dt.recom[, RM_P_2 := gsub("_S_P", "", second_P)]
-  dt.recom[, RM_P_3 := gsub("_S_P", "", third_P)]
-  
-  ## when the score of the selected measure is <=0, discard the advice 
-  dt.recom[first_P_score <= 0, RM_P_1 := "no suitable advice"]
-  dt.recom[second_P_score <= 0, RM_P_2 := "no suitable advice"]
-  dt.recom[third_P_score <= 0, RM_P_3 := "no suitable advice"]
-  
-  ## when no indicator is below the threshold level, give no advice.
-  # count number of indicators which are below threshold level
-  cols = colnames(dt.recom)[grepl("Dremp_P_", colnames(dt.recom))]
-  dt.recom[, nr_blth_P := rowSums(.SD), .SDcols = cols]
-  dt.recom[nr_blth_P == 0, c("RM_P_1", "RM_P_2", "RM_P_3")  := "no advice needed"]
-  
-  ## Biological recommondations ----------------------------------------------
-  # Choose best 3 measures for biological indicators
-  # In case of ties, the one with most left (i.e. smallest 'Order' value) is taken first.
-  cols = colnames(dt.recom)[grepl("_S_B", colnames(dt.recom))]
-  dt.recom[, first_B := colnames(.SD)[apply(.SD, 1, function(x) which(rank(x, ties.method = 'last') == length(cols)))], .SDcols = cols]
-  dt.recom[, second_B := colnames(.SD)[apply(.SD, 1, function(x) which(rank(x, ties.method = 'last') == length(cols) - 1))], .SDcols = cols]
-  dt.recom[, third_B := colnames(.SD)[apply(.SD, 1, function(x) which(rank(x, ties.method = 'last') == length(cols) - 2))], .SDcols = cols]
-  
-  # Store scores of the 1st, 2nd, and 3rd measures
-  dt.recom[, first_B_score := dt.recom[[first_B]][.I], by=first_B]
-  dt.recom[, second_B_score := dt.recom[[second_B]][.I], by=second_B]
-  dt.recom[, third_B_score := dt.recom[[third_B]][.I], by=third_B]
-  
-  # Get measure names to recommend (e.g. "M3")
-  dt.recom[, RM_B_1 := gsub("_S_B", "", first_B)]
-  dt.recom[, RM_B_2 := gsub("_S_B", "", second_B)]
-  dt.recom[, RM_B_3 := gsub("_S_B", "", third_B)]
-  
-  ## when the score of the selected measure is <=0, discard the advice 
-  dt.recom[first_B_score <= 0, RM_B_1 := "no suitable advice"]
-  dt.recom[second_B_score <= 0, RM_B_2 := "no suitable advice"]
-  dt.recom[third_B_score <= 0, RM_B_3 := "no suitable advice"]
-  
-  ## when no indicator is below the threshold level, give no advice.
-  # count number of indicators which are below threshold level
-  cols = colnames(dt.recom)[grepl("Dremp_B_", colnames(dt.recom))]
-  dt.recom[, nr_blth_B := rowSums(.SD), .SDcols = cols]
-  dt.recom[nr_blth_B == 0, c("RM_B_1", "RM_B_2", "RM_B_3")  := "no advice needed"]
-  
-  
-  # remove unnecessary columns
-  dt.recom[, (colnames(dt.recom)[grepl("first|second|third|Esc_|Dremp_|nr_blth_", colnames(dt.recom))]) := NULL]
-  
-  if(extensive == FALSE){
-    # remove evaluation of each measure on each soil indicator (e.g. M1_)
-    dt.recom[, (colnames(dt.recom)[grepl("^M", colnames(dt.recom))]) := NULL]
-  }
+ 
   
   return(dt.recom)
   
