631a583b85f937c3046928cf2f599f69bb678573
Brent <brent.riechelman@xs4all.nl>
2021-01-27 10:07:38 +0100

    fixed convert_B_OV_WENR() after checks

--- R/convert_ov_wenr.R
+++ R/convert_ov_wenr.R
@@ -10,24 +10,28 @@
 
 convert_B_OV_WENR <- function(B_OV_WENR) {
   # Create data table
-  dt <- data.table(B_OV_WENR = B_OV_WENR)
+  dt <- data.table(B_OV_WENR = B_OV_WENR, B_OV_WENR_new = "")
   if(is.numeric(dt$B_OV_WENR)){
     checkmate::assert_subset(B_OV_WENR, empty.ok = FALSE, choices = c(1, 2, 3, 4, 5, 10, 11, 401, 901, 902))
 
     # Convert numbers to appropriate strings
-    dt[B_OV_WENR==1,B_OV_WENR := 'Zeer beperkt']
-    dt[B_OV_WENR==2,B_OV_WENR := 'Beperkt']
-    dt[B_OV_WENR==3,B_OV_WENR := 'Matig']
-    dt[B_OV_WENR==4,B_OV_WENR := 'Groot']
-    dt[B_OV_WENR==5,B_OV_WENR := 'Zeer groot']
-    dt[B_OV_WENR==901,B_OV_WENR := 'Bebouwing en infrastructuur']
-    dt[B_OV_WENR==902,B_OV_WENR := 'Water']
-    dt[B_OV_WENR==401,B_OV_WENR := 'Glastuinbouw, niet beoordeeld']
-    dt[B_OV_WENR==10,B_OV_WENR := 'Beperkt door veenlagen']
-    dt[B_OV_WENR==11,B_OV_WENR := 'Van nature dicht']
+    dt[B_OV_WENR==1,B_OV_WENR_new := 'Zeer beperkt']
+    dt[B_OV_WENR==2,B_OV_WENR_new := 'Beperkt']
+    dt[B_OV_WENR==3,B_OV_WENR_new := 'Matig']
+    dt[B_OV_WENR==4,B_OV_WENR_new := 'Groot']
+    dt[B_OV_WENR==5,B_OV_WENR_new := 'Zeer groot']
+    dt[B_OV_WENR==901,B_OV_WENR_new := 'Bebouwing en infrastructuur']
+    dt[B_OV_WENR==902,B_OV_WENR_new := 'Water']
+    dt[B_OV_WENR==401,B_OV_WENR_new := 'Glastuinbouw, niet beoordeeld']
+    dt[B_OV_WENR==10,B_OV_WENR_new := 'Beperkt door veenlagen']
+    dt[B_OV_WENR==11,B_OV_WENR_new := 'Van nature dicht']
+    
+    # set B_OV_WENR to converted values
+    B_OV_WENR <- dt[,B_OV_WENR_new]
+  } else {
+    # Use unaltered data for B_OV_WENR if not numeric
+    B_OV_WENR <- dt[,B_OV_WENR]
   }
-  
-  B_OV_WENR <- dt[,B_OV_WENR]
   
   # return value
   return(B_OV_WENR)
