2e0acde54e0519bc4467cc21d21997330968da8d
Brent <brent.riechelman@xs4all.nl>
2021-03-22 12:15:47 +0100

    initial structure of workability script

---
+++ R/workability.R
@@ -0,0 +1,23 @@
+#' Calculate indicator for workability 
+#'
+#' This function calculates the workability of soils, given as potential length of the growing season. Based on Huinink (2018)
+#' 
+#' @param A_CLAY_MI (numeric) The clay content of the soil (in procent)
+#' @param A_SILT_MI (numeric) The silt content of the soil (in procent)
+#' @param B_LU_BRP (numeric) The crop code (gewascode) from the BRP
+#'  
+#' @export
+calc_workability <- function() {
+  
+}
+#' Calculate indicator for workability
+#'
+#' This function calculates the indicator for the workability of the soil expressed as the period in which the soil can be worked without
+#' inflicting structural damage that cannot be restored by the regular management on the farm.
+#'  
+#' @param D_P_WO (numeric) The value for workability as calculated by \code{\link{calc_workability}}
+#' 
+#' @export
+ind_workability <- function() {
+  
+}
