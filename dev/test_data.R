af981375dcda059d4d935ae943402bbed411dace
Brent <brent.riechelman@xs4all.nl>
2021-03-23 11:13:43 +0100

    some data for testing

---
+++ dev/test_data.R
@@ -0,0 +1,10 @@
+# Script to make some test data to test workability functions
+
+# 1
+A_CLAY_MI <- 28
+A_SILT_MI <- 20
+B_LU_BRP <- 212
+B_BT_AK <-  'zeeklei'
+B_GT <- 'GtIII'
+B_GLG <- 90
+B_GHG <- 35
