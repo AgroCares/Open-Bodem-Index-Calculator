b2225accaf39678a80b853fd3c21886697615c17
Brent <brent.riechelman@xs4all.nl>
2021-03-24 14:48:27 +0100

    Revert "testing"
    This reverts commit ad338573ce2cd95f1ff8882ab0ef58230653305a.

--- tests/testthat/test-workability.R
+++ tests/testthat/test-workability.R
@@ -1,6 +1,6 @@
 test_that("calc_workability works", {
   expect_equal(
-    calc_workability(
+    calc_waterstressindex(
       A_CLAY_MI = c(28, 28, 28, 28, 28, 28, 10, 0),
       A_SILT_MI = c(20, 20, 20, 20, 20, 20, 20, 14),
       B_LU_BRP = c(238, 240, 370, 238, 980, 259, 265, 1929),
@@ -8,7 +8,7 @@ test_that("calc_workability works", {
       B_GLG = c(90, 90, 90, 80, 90, 80, 105, 150),
       B_GHG = c(35, 35, 35, 30, 35, 50, 15, 25)
     ),
-    expected = c(0.75,0.90,1,1,0.82, 0.939, 0.635,0.953),
+    expected = c(0.75,0.90,1,1,0.82, 1, 0.635,0.546),
     tolerance = .01
   )
 })
