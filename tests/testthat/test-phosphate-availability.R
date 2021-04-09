5759f5b2049459b20aa5fe48600f2ce1f3829ef7
Gerard <gerard.ros@nmi-agro.nl>
2021-04-09 16:04:48 +0200

    update check element names for P

--- tests/testthat/test-phosphate-availability.R
+++ tests/testthat/test-phosphate-availability.R
@@ -1,8 +1,8 @@
 test_that("calc_phosphate_availability works", {
   expect_equal(
     calc_phosphate_availability(
-      A_P_PAL = 25,
-      A_P_PAE = 1.5,
+      A_P_AL = 25,
+      A_P_CC = 1.5,
       A_P_WA = 24,
       B_LU_BRP = 2014
     ),
@@ -11,8 +11,8 @@ test_that("calc_phosphate_availability works", {
   )
   expect_equal(
     calc_phosphate_availability(
-      A_P_PAL = c(25, 20),
-      A_P_PAE = c(1.5, 3),
+      A_P_AL = c(25, 20),
+      A_P_CC = c(1.5, 3),
       A_P_WA = c(24,20),
       B_LU_BRP = c(2014, 265)
     ),
