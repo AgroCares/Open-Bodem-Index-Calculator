f9917693d836ceecb9f26fd5345ca9b65e5be556
Gerard <gerard.ros@nmi-agro.nl>
2021-04-10 05:31:49 +0200

    update check funs for obic_format_input

--- tests/testthat/test-format_gwt.R
+++ tests/testthat/test-format_gwt.R
@@ -1,19 +1,19 @@
 test_that("format_gwt works", {
   expect_equal(
     format_gwt(
-      B_GT = c("II", "VI", "IIb")
+      B_GWL_CLASS = c("II", "VI", "IIb")
     ),
     expected = c("GtII", "GtVI", "GtIIb")
   )
   expect_equal(
     format_gwt(
-      B_GT = c("GtIII", "GtVI", "GtIIb")
+      B_GWL_CLASS = c("GtIII", "GtVI", "GtIIb")
     ),
     expected = c("GtIII", "GtVI", "GtIIb")
   )
   expect_equal(
     format_gwt(
-      B_GT = c("II", "VI", "IIb", "GtIII", "GtIIIb")
+      B_GWL_CLASS = c("II", "VI", "IIb", "GtIII", "GtIIIb")
     ),
     expected = c("GtII", "GtVI", "GtIIb", "GtIII", "GtIIIb")
   )
