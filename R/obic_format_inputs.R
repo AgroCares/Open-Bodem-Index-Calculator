ccd940d0360d6f9aa9f4add91bcea7d3c667f95e
Gerard <gerard.ros@nmi-agro.nl>
2021-04-28 11:05:31 +0200

    add format_aer to allow both names and db codes for B_AER_CBS

--- R/obic_format_inputs.R
+++ R/obic_format_inputs.R
@@ -57,3 +57,40 @@ format_soilcompaction <- function(B_SC_WENR) {
   # return value
   return(B_SC_WENR)
 }
+
+#' Convert possible B_AER_CBS values to standardized values
+#' 
+#' This function formats information of Agricultural Economic Region so it can be understood by other OBIC functions
+#' 
+#' @param B_AER_CBS (character) The agricultural economic region in the Netherlands (CBS, 2016)
+#' 
+#' @import data.table
+#' 
+#' @export
+format_aer <- function(B_AER_CBS) {
+  
+  # options for B_AER_CBS
+  aer.text <- c('Zuid-Limburg','Zuidelijk Veehouderijgebied','Zuidwest-Brabant',
+                 'Zuidwestelijk Akkerbouwgebied','Rivierengebied','Hollands/Utrechts Weidegebied',
+                 'Waterland en Droogmakerijen','Westelijk Holland','IJsselmeerpolders',
+                 'Centraal Veehouderijgebied','Oostelijk Veehouderijgebied','Noordelijk Weidegebied',
+                 'Veenkoloni\xebn en Oldambt','Bouwhoek en Hogeland')
+  
+  # options for B_AER_CBS
+  aer.code <- c("LG14","LG13","LG12","LG11","LG10","LG09","LG08","LG07","LG06","LG05","LG04","LG03","LG02","LG01")
+  
+  # all input options
+  aer.all <- c(aer.text,aer.code)
+  
+  # Check if B_GT values are appropriate
+  checkmate::assert_subset(B_AER_CBS, empty.ok = FALSE, choices = aer.all)
+  
+  # which of the input values are database codes
+  var.sel <- match(B_AER_CBS,aer.code,nomatch = 0)
+  
+  # replace numeric values with strings
+  B_AER_CBS[B_AER_CBS %in% aer.code] <- aer.text[var.sel]
+  
+  # Return B_AER_CBS
+  return(B_AER_CBS)
+}
