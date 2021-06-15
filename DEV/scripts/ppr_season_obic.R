885fa5597448f7038fe6de6c587e8498de0f0f60
Brent <brent.riechelman@xs4all.nl>
2021-06-15 10:21:38 +0200

    added soil specific mais workability season

--- DEV/scripts/ppr_season_obic.R
+++ DEV/scripts/ppr_season_obic.R
@@ -7,5 +7,24 @@
   # Not all landuses in the original table appear in brp and some of the brp crops have been assigned to "overig" and could be reclassified later.
   season.obic <- fread('dev/data/season_obic.csv')
   
+  # remove column numbers
+  season.obic <- season.obic[,.(landuse, req_days_pre_glg, req_days_post_glg, total_days, derving)]
+  
+  # add soiltype columns to be molten based on soils.obic$soiltype.m
+  season.obic <- season.obic[, zand := 'zand']
+  season.obic <- season.obic[, klei := 'klei']
+  season.obic <- season.obic[, veen := 'veen']
+  season.obic <- season.obic[, loess := 'loess']
+  
+  # melt soiltype
+  season.obic <- melt(season.obic, measure.vars = c('zand', 'klei', 'veen', 'loess'), variable.name = 'soiltype.m')
+  
+  # remove value column (only column names were really needed in melting)
+  season.obic <- season.obic[,.(landuse, req_days_pre_glg, req_days_post_glg, total_days, derving, soiltype.m)]
+  
+  # set req days post glg for mais on sand/loess to match 1 october and other soils to 20 october
+  season.obic <- season.obic[landuse == 'snijmais',req_days_post_glg := 
+                               fifelse(soiltype.m %in% c('zand', 'loess'), 274-227, 293-227)] # where numbers are doy resulting in number of days after glg (doy = 227)
+  
   # save table
   save(season.obic,file = 'data/season.obic.RData')
