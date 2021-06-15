6935fafb03d2a921c57acaf4e37d24c801bbf082
Brent <brent.riechelman@xs4all.nl>
2021-06-15 10:51:24 +0200

    added test

--- tests/testthat/test-workability.R
+++ tests/testthat/test-workability.R
@@ -1,15 +1,15 @@
 test_that("calc_workability works", {
   expect_equal(
     calc_workability(
-      A_CLAY_MI = c(15.6,13.6, 4.3, 22.6, 1.9, 2.9, 3.1, 4.3, 15.6),
-      A_SILT_MI = c(16.7,30.5, 11.8, 36.6, 9.2, 8.6, 10.6, 11.8, 16.7),
-      B_LU_BRP = c(233, 234, 236, 256, 259, 265, 265, 317, 2014),
-      B_SOILTYPE_AGR = c('zeeklei','zeeklei', 'dekzand','zeeklei', 'dekzand', 'dekzand', 'veen', 'dekzand', 'zeeklei'),
-      B_GWL_GLG = c(173,139, 106, 144, 115, 113, 42, 106, 173),
-      B_GWL_GHG = c(21, 18, 62, 70, 49, 81, 9, 62, 21),
-      B_Z_TWO = c(400, 400, 400, 400, 400, 400, 400, 400, 400)
+      A_CLAY_MI = c(15.6,13.6, 4.3, 22.6, 1.9, 2.9, 3.1, 4.3, 15.6, 1.9),
+      A_SILT_MI = c(16.7,30.5, 11.8, 36.6, 9.2, 8.6, 10.6, 11.8, 16.7, 9.2),
+      B_LU_BRP = c(233, 234, 236, 256, 259, 265, 265, 317, 2014, 259),
+      B_SOILTYPE_AGR = c('zeeklei','zeeklei', 'dekzand','zeeklei', 'dekzand', 'dekzand', 'veen', 'dekzand', 'zeeklei', 'maasklei'),
+      B_GWL_GLG = c(173,139, 106, 144, 115, 113, 42, 106, 173, 115),
+      B_GWL_GHG = c(21, 18, 62, 70, 49, 81, 9, 62, 21, 49),
+      B_Z_TWO = c(400, 400, 400, 400, 400, 400, 400, 400, 400, 400)
     ),
-    expected = c(0.57,0.45,0.54,0.84,0.78, 1, 0.00,0.74,0.66),
+    expected = c(0.57,0.45,0.54,0.84,0.78, 1, 0.00,0.74,0.66, 0.59),
     tolerance = .01
   )
 })
@@ -23,4 +23,25 @@ test_that("ind_workability works", {
     expected = c(0, 0.5, 1, 0, 0.5, 1),
     tolerance = .015
   )
+})
+
+test_that('diffrenttiation maize on sand vs mais on clay workabilty season works',{
+  expect_true(calc_workability(
+    A_CLAY_MI = c( 1.9),
+    A_SILT_MI = c(9.2),
+    B_LU_BRP = c(259),
+    B_SOILTYPE_AGR = c( 'dekzand'),
+    B_GWL_GLG = c(115),
+    B_GWL_GHG = c(49),
+    B_Z_TWO = c(400)
+  )>
+    calc_workability(
+      A_CLAY_MI = c( 1.9),
+      A_SILT_MI = c(9.2),
+      B_LU_BRP = c(259),
+      B_SOILTYPE_AGR = c( 'zeeklei'),
+      B_GWL_GLG = c(115),
+      B_GWL_GHG = c(49),
+      B_Z_TWO = c(400)
+    ))
 })
