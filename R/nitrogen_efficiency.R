897b76edff24e1f56c0035b8b6b6833f68fe1550
KeesDoolNMI <kees.vandendool@nmi-agro.nl>
2021-08-06 15:18:34 +0200

    Simplify script and add indicator function

--- R/nitrogen_efficiency.R
+++ R/nitrogen_efficiency.R
@@ -8,27 +8,20 @@
 #' @param B_AER_CBS (character) The agricultural economic region in the Netherlands (CBS, 2016) 
 #' @param A_SOM_LOI (numeric) The percentage organic matter in the soil (\%)
 #' @param A_CLAY_MI (numeric) The clay content of the soil (\%)
-#' @param A_N_RT (numeric) The organic nitrogen content of the soil in mg N / kg
-#' @param A_CN_FR (numeric) The carbon to nitrogen ratio
-#' @param D_GA (numeric) The age of the grass if present
-#' @param A_P_AL (numeric) The P-AL content of the soil
-#' @param A_P_CC (numeric) The plant available P content, extracted with 0.01M CaCl2 (mg / kg)
-#' @param A_P_WA (numeric) The P-content of the soil extracted with water (mg P2O5 / 100 ml soil)
-#' @param A_CEC_CO (numeric) The cation exchange capacity of the soil (mmol+ / kg), analysed via Cobalt-hexamine extraction
-#' @param A_K_CO_PO (numeric) The occupation of the CEC with K (\%)
-#' @param A_K_CC (numeric) The plant available K content, extracted with 0.01M CaCl2 (mg / kg)(\%)
-#' @param A_PH_CC (numeric) The acidity of the soil, measured in 0.01M CaCl2 (-)
+#' @param D_PBI (numeric) The value of phosphate availability calculated by \code{\link{calc_phosphate_availability}}
+#' @param D_K (numeric) The value of K-index calculated by \code{\link{calc_potassium_availability}}
+#' @param D_PH_DELTA (numeric) The pH difference with the optimal pH.
+#' @param D_NLV (numeric) The value of NLV  calculated by \code{\link{calc_nlv}} 
 #' @param M_GREEN (boolean) A soil measure. Are catch crops sown after main crop (optional, option: yes or no)
-#' @param norm_fr (numeric) The fraction of the application norm utilized
+#' @param B_FERT_NORM_FR (numeric) The fraction of the application norm utilized
 #' 
 #'         
 #' @export
-calc_n_efficiency <- function(B_LU_BRP,B_SOILTYPE_AGR,B_GWL_CLASS,B_AER_CBS,A_SOM_LOI,A_CLAY_MI,A_N_RT,A_CN_FR,D_GA,
-                              A_P_AL,A_P_CC,A_P_WA,A_CEC_CO,A_K_CO_PO,A_K_CC,A_PH_CC,M_GREEN,norm_fr){
-  
+calc_n_efficiency <- function(B_LU_BRP, B_SOILTYPE_AGR, B_GWL_CLASS, B_AER_CBS, A_SOM_LOI, A_CLAY_MI,
+                              D_PBI, D_K, D_PH_DELTA, D_NLV, M_GREEN = FALSE, B_FERT_NORM_FR = TRUE){
   
   # Check inputs
-  arg.length <- max(length(B_LU_BRP),length(norm_fr))
+  arg.length <- max(length(B_LU_BRP),length(B_SOILTYPE_AGR),length(B_GWL_CLASS),length(B_AER_CBS),length(B_FERT_NORM_FR), length(M_GREEN))
   
   # Check B parameters
   checkmate::assert_numeric(B_LU_BRP, any.missing = FALSE, min.len = 1, len = arg.length)
@@ -36,7 +29,7 @@ calc_n_efficiency <- function(B_LU_BRP,B_SOILTYPE_AGR,B_GWL_CLASS,B_AER_CBS,A_SO
   checkmate::assert_character(B_SOILTYPE_AGR, any.missing = FALSE, len = arg.length)
   checkmate::assert_subset(B_SOILTYPE_AGR, choices = unique(soils.obic$soiltype))
   checkmate::assert_character(B_GWL_CLASS,any.missing = FALSE, len = arg.length)
-  checkmate::assert_subset(B_AER_CBS, choices = c('GtI','GtII','GtIII','GtIV','GtV','GtVI','GtVII','GtVIII',
+  checkmate::assert_subset(B_GWL_CLASS, choices = c('GtI','GtII','GtIII','GtIV','GtV','GtVI','GtVII','GtVIII',
                                                   'GtIIb','GtIIIb','GtVb'), empty.ok = FALSE)
   checkmate::assert_character(B_AER_CBS, any.missing = FALSE, min.len = 1, len = arg.length)
   checkmate::assert_subset(B_AER_CBS, choices = c('Zuid-Limburg','Zuidelijk Veehouderijgebied','Zuidwest-Brabant',
@@ -44,38 +37,13 @@ calc_n_efficiency <- function(B_LU_BRP,B_SOILTYPE_AGR,B_GWL_CLASS,B_AER_CBS,A_SO
                                                   'Waterland en Droogmakerijen','Westelijk Holland','IJsselmeerpolders',
                                                   'Centraal Veehouderijgebied','Oostelijk Veehouderijgebied','Noordelijk Weidegebied',
                                                   'Veenkoloni\u00EBn en Oldambt','Veenkolonien en Oldambt','Bouwhoek en Hogeland'), empty.ok = FALSE)
+  checkmate::assert_numeric(B_FERT_NORM_FR, lower = 0, upper = 1, any.missing = FALSE, len = arg.length)
   
   # Check SOM and CLAY
   checkmate::assert_numeric(A_SOM_LOI, lower = 0.5, upper = 75, any.missing = FALSE, len = arg.length)
   checkmate::assert_numeric(A_CLAY_MI, lower = 0.1, upper = 75, any.missing = FALSE)
   
-  # Check N values
-  checkmate::assert_numeric(A_N_RT, lower = 0.1, upper = 30000, any.missing = FALSE, len = arg.length)
-  checkmate::assert_numeric(A_CN_FR, lower = 0.1, upper = 50, any.missing = FALSE, len = arg.length)
-  checkmate::assert_numeric(D_GA, lower = 0, upper = 99, len = arg.length)
-
-  # check P input parameters for grassland 
-  arg.length.grass <- max(length(A_P_AL),length(A_P_CC))
-  if(arg.length.grass > 0){
-    checkmate::assert_numeric(A_P_AL, lower = 1, upper = 250, any.missing = FALSE, len = arg.length.grass)
-    checkmate::assert_numeric(A_P_CC, lower = 0.1, upper = 100, any.missing = FALSE, len = arg.length.grass)
-  }
-  
-  # check P input parameters for arable soils
-  arg.length.arable <- length(A_P_WA)
-  if(arg.length.arable > 0){
-    checkmate::assert_numeric(A_P_WA, lower = 1, upper = 250, any.missing = FALSE, len = arg.length.arable)  
-  }
-  
-  # Check Potasium values
-  checkmate::assert_numeric(A_K_CC, lower = 0, upper = 600, any.missing = FALSE, len = arg.length)
-  checkmate::assert_numeric(A_K_CO_PO, lower = 0.1, upper = 50, any.missing = FALSE, len = arg.length)
-  checkmate::assert_numeric(A_CEC_CO, lower = 1, upper = 1000, any.missing = FALSE, len = arg.length)  
-  
-  # Check pH
-  checkmate::assert_numeric(A_PH_CC, lower = 3, upper = 10, any.missing = FALSE, len = arg.length)
-  
-  checkmate::assert_numeric(norm_fr, lower = 0.1, upper = 1, any.missing = FALSE, len = arg.length)
+  checkmate::assert_logical(M_GREEN, any.missing = FALSE, len = arg.length)
   
   
   # Import data into table
@@ -86,18 +54,12 @@ calc_n_efficiency <- function(B_LU_BRP,B_SOILTYPE_AGR,B_GWL_CLASS,B_AER_CBS,A_SO
                    B_AER_CBS = B_AER_CBS,
                    A_SOM_LOI = A_SOM_LOI,
                    A_CLAY_MI = A_CLAY_MI,
-                   A_N_RT = A_N_RT,
-                   A_CN_FR = A_CN_FR,
-                   A_P_PAL = A_P_PAL,
-                   A_P_CC = A_P_CC,
-                   A_P_WA = A_P_WA,
-                   A_CEC_CO = A_CEC_CO,
-                   A_K_CO_PO = A_K_CO_PO, 
-                   A_K_CC = A_K_CC,
-                   A_PH_CC = A_PH_CC,
-                   D_GA = D_GA,
+                   D_PBI = D_PBI,
+                   D_K = D_K,
+                   D_PH_DELTA = D_PH_DELTA,
+                   D_NLV = D_NLV,
                    M_GREEN = M_GREEN,
-                   norm_fr = norm_fr
+                   B_FERT_NORM_FR = B_FERT_NORM_FR
                    )
   
   
@@ -133,54 +95,52 @@ calc_n_efficiency <- function(B_LU_BRP,B_SOILTYPE_AGR,B_GWL_CLASS,B_AER_CBS,A_SO
   cols <- colnames(dt)[grepl('^nf_',colnames(dt))]
   dt[,(cols) := NULL]
   
-  
-  # Calculate bulk density and OC content of the soil
-  dt[, D_BDS := calc_bulk_density(B_SOILTYPE_AGR,A_SOM_LOI,A_CLAY_MI)]
-  dt[, D_RD := calc_root_depth(B_LU_BRP)]
-  dt[, D_OC := calc_organic_carbon(A_SOM_LOI, D_BDS, D_RD)]
-  
-  # Calculate crop rotation fractions
-  dt[, D_CP_STARCH := calc_rotation_fraction(ID, B_LU_BRP, crop = "starch")]
-  dt[, D_CP_POTATO := calc_rotation_fraction(ID, B_LU_BRP, crop = "potato")]
-  dt[, D_CP_SUGARBEET := calc_rotation_fraction(ID, B_LU_BRP, crop = "sugarbeet")]
-  dt[, D_CP_GRASS := calc_rotation_fraction(ID, B_LU_BRP, crop = "grass")]
-  dt[, D_CP_MAIS := calc_rotation_fraction(ID, B_LU_BRP, crop = "mais")]
-  dt[, D_CP_OTHER := calc_rotation_fraction(ID, B_LU_BRP, crop = "other")]
-  
-  
   # Calculate P, K and pH status
-  dt[,D_PBI := calc_phosphate_availability(B_LU_BRP, A_P_PAL, A_P_CC,A_P_WA)]
   dt[,I_P := ind_phosphate_availability(D_PBI)]
-  
-  dt[,D_K := calc_potassium_availability(B_LU_BRP, B_SOILTYPE_AGR, A_SOM_LOI, A_CLAY_MI, A_PH_CC,
-                                         A_CEC_CO, A_K_CO_PO, A_K_CC)]
   dt[,I_K := ind_potassium(D_K,B_LU_BRP, B_SOILTYPE_AGR, A_SOM_LOI)]
-  
-  dt[,D_PH_DELTA := calc_ph_delta(B_LU_BRP, B_SOILTYPE_AGR, A_SOM_LOI, A_CLAY_MI, A_PH_CC,
-                                  D_CP_STARCH, D_CP_POTATO, D_CP_SUGARBEET, D_CP_GRASS, D_CP_MAIS, D_CP_OTHER)]
   dt[,I_PH := ind_ph(D_PH_DELTA)]
   
   
   # Determine correction factor if P, K and pH are optimal
   dt[,cf_pkph := fifelse(I_P > 0.9 & I_K > 0.9 & I_PH > 0.9,0.9,1) ]
   
-  # Calculate N sources
+  # Set N deposition
   dt[,deposition := 25] # Fixed value
-  dt[,nlv := calc_nlv(B_LU_BRP, B_SOILTYPE_AGR, A_N_RT, A_CN_FR, D_OC, D_BDS, D_GA)]
   
   # Calcualte the N application space according to the norm
-  dt[,n_space := (1 - norm_fr) * n_eff]
+  dt[,n_space := (1 - B_FERT_NORM_FR) * n_eff]
   
   # N uptake by catchcrop
   # Check values https://www.wur.nl/upload_mm/c/8/1/6b63d919-1690-4f07-981a-07b3b6a3e7f1_1705577_Oene%20Oenema%20bijlage%201.pdf
   dt[,catchcrop := fifelse(M_GREEN, 75,0)]
   
   # Calculate N surplus
-  dt[,n_sp := max(0,((deposition + nlv) - n_space - catchcrop) * cf_pkph)]
+  dt[,n_sp := max(0,((deposition + D_NLV) - n_space - catchcrop) * cf_pkph)]
   
   # compute (potential and soil derived) N leaching to groundwater D_NGW (mg NO3/L) 
-  dt[, value := nf * n_sp]
+  dt[, D_NLEACH := nf * n_sp]
   
-  return(dt[,value])
+  return(dt[,D_NLEACH])
   
 }
+
+
+
+#' Calculate an indicator value for nitrogen use efficiency and leaching based on N surplus
+#' 
+#' This function gives an indicator value for nitrogen use efficiency calculated by \code{\link{calc_n_efficiency}}, this function makes use of \code{\link{ind_nretention}}
+#' 
+#' @param D_NLEACH (numeric) The value of N leaching calculated by \code{\link{calc_n_efficiency}}
+#' 
+#'         
+#' @export
+ind_n_efficiency <- function(D_NLEACH){
+  
+    # Evaluate the N retention for groundwater 
+    value <- ind_nretention(D_NLEACH,'gw')
+   
+  
+  return(value)
+  
+}
+
--- R/pesticides.R
+++ R/pesticides.R
@@ -1,6 +1,6 @@
 #' Calculate risk of pesticide leaching
 #' 
-#' This function calculates the volume of water (in mm / ha) that infiltrates to the groundwater given the crop rotation plan.
+#' This function calculates the risk of pesticide leaching from a soil. The risk is calculated by comparing the current leached fraction with a worst case scenario
 #' 
 #' 
 #' @param B_SOILTYPE_AGR (character) The agricultural type of soil
@@ -11,10 +11,18 @@
 #' @param D_PSP (numeric) The precipitation surplus per crop  calculated by \code{\link{calc_gw_recharge}}
 #' 
 #' @export
-calc_pesticide_leaching <- function(B_SOILTYPE_AGR,A_SOM_LOI,A_SAND_MI,A_SILT_MI,A_CLAY_MI,D_PSP) {
+calc_pesticide_leaching <- function(B_SOILTYPE_AGR, A_SOM_LOI, A_CLAY_MI, A_SAND_MI, A_SILT_MI, D_PSP) {
   
   # check inputs
+  arg.length <- max(length(B_SOILTYPE_AGR),length(A_SOM_LOI),length(A_CLAY_MI),length(A_SAND_MI),length(A_SILT_MI),length(D_PSP))
   
+  checkmate::assert_character(B_SOILTYPE_AGR, any.missing = FALSE, len = arg.length)
+  checkmate::assert_subset(B_SOILTYPE_AGR, choices = unique(soils.obic$soiltype))
+  checkmate::assert_numeric(A_SOM_LOI, lower = 0.5, upper = 75, any.missing = FALSE, len = arg.length)
+  checkmate::assert_numeric(A_CLAY_MI, lower = 0.1, upper = 75, any.missing = FALSE, len = arg.length)
+  checkmate::assert_numeric(A_SAND_MI, lower = 0.1, upper = 75, any.missing = FALSE, len = arg.length)
+  checkmate::assert_numeric(A_SILT_MI, lower = 0.1, upper = 75, any.missing = FALSE, len = arg.length)
+  checkmate::assert_numeric(D_PSP, lower = 0, upper = 1000, any.missing = FALSE, len = arg.length)
   
   # import data in table
   dt <- data.table(B_SOILTYPE_AGR = B_SOILTYPE_AGR,
@@ -28,10 +36,10 @@ calc_pesticide_leaching <- function(B_SOILTYPE_AGR,A_SOM_LOI,A_SAND_MI,A_SILT_MI
   
   ## Minimal situation
   # Calcualte bulk density
-  dt[,BD_MIN := calc_bulk_density(B_SOILTYPE_AGR,A_SOM_LOI,A_CLAY_MI)]
+  dt[,BD_MIN := calc_bulk_density(B_SOILTYPE_AGR,A_SOM_LOI = rep(0.5,arg.length),A_CLAY_MI)/1000]
   
   # Calculate volume fraction of water
-  dt[,vfw_min := calc_waterretention(A_CLAY_MI,A_SAND_MI,A_SILT_MI,A_SOM_LOI = 1,type = 'water holding capacity')]
+  dt[,vfw_min := calc_waterretention(A_CLAY_MI,A_SAND_MI,A_SILT_MI,A_SOM_LOI = 0.5,type = 'water holding capacity')]
   
   # Calculate water flux
   dt[,B_WATER_FLUX_MIN := D_PSP / 365 / vfw_min / 100]
@@ -42,7 +50,7 @@ calc_pesticide_leaching <- function(B_SOILTYPE_AGR,A_SOM_LOI,A_SAND_MI,A_SILT_MI
   
   ## Current situation
   # Calcualte bulk density
-  dt[,BD := calc_bulk_density(B_SOILTYPE_AGR,A_SOM_LOI,A_CLAY_MI)]
+  dt[,BD := calc_bulk_density(B_SOILTYPE_AGR,A_SOM_LOI,A_CLAY_MI)/1000]
   
   # Calculate volume fraction of water
   dt[,vfw := calc_waterretention(A_CLAY_MI,A_SAND_MI,A_SILT_MI,A_SOM_LOI,type = 'water holding capacity')]
@@ -53,11 +61,42 @@ calc_pesticide_leaching <- function(B_SOILTYPE_AGR,A_SOM_LOI,A_SAND_MI,A_SILT_MI
   # Calculate pesticide leaching fraction
   dt[,pest_leach := exp((-0.34/60 * (vfw + BD * A_SOM_LOI/100 * 10)/B_WATER_FLUX))]
   
-  dt[,D_PEST := log(pest_leach)/log(pest_leach_min)]
+  dt[,D_PESTICIDE := pest_leach/pest_leach_min]
   
-  return(dt[,D_PEST])
+  return(dt[,D_PESTICIDE])
   
 }
 
 
+#' Calculate an indicator score for pesticide leaching
+#' 
+#' This function calculates the indicator value for pesticide leaching from a soil
+#' 
+#' 
+#' @param D_PESTICIDE The fraction of pesticide leached compared to the worst case scenario
+#' @param B_GWL_CLASS (character) The groundwater table class
+#' 
+#' @export
+ind_pesticide_leaching <- function(D_PESTICIDE, B_GWL_CLASS) {
 
+  # Check inputs
+  arg.length <- max(length(D_PESTICIDE),length(B_GWL_CLASS))
+  
+  checkmate::assert_numeric(D_PESTICIDE, lower = 0.1, upper = 1, any.missing = FALSE, len = arg.length)
+  checkmate::assert_character(B_GWL_CLASS, any.missing = FALSE, min.len = 1, len = arg.length)
+  checkmate::assert_subset(B_GWL_CLASS, choices = c('GtI','GtII','GtIII','GtIV','GtV','GtVI','GtVII','GtVIII',
+                                                    'GtIIb','GtIIIb','GtVb'), empty.ok = FALSE)
+  
+  # Load data into table
+  dt <- data.table(D_PESTICIDE = D_PESTICIDE,
+                   B_GWL_CLASS = B_GWL_CLASS)
+  
+  # Calculate indicator score based on B_GWL_CLASS
+  dt[,I_PESTICIDE := fifelse(B_GWL_CLASS %in% c('GtI','GtII','GtIIb','GtIII','GtIIIb'),
+                             pmin(1.1 * evaluate_logistic(D_PESTICIDE, 20, 0.8, 1.5, increasing = FALSE), 1),
+                             evaluate_logistic(D_PESTICIDE, 20, 0.8, 1.5, increasing = FALSE))]
+  
+  
+ return(dt[,I_PESTICIDE])
+  
+}
