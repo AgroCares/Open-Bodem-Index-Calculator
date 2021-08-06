b8de46cc390c9076699644a875028f1747cf8a5c
KeesDoolNMI <kees.vandendool@nmi-agro.nl>
2021-08-06 15:21:39 +0200

    Update loading ppr_makkink

--- DEV/scripts/ppr_makkink.R
+++ DEV/scripts/ppr_makkink.R
@@ -1,15 +1,15 @@
 # make makkink table
 
   # load csv file with makkink per crop per month
-  d1 <- fread('dev/data/makkink.csv')
+  dt <- fread('dev/data/makkink.csv', header = TRUE)
   
   # colnames for the 12 months
-  cols <- colnames(d1)[-1]
+  cols <- colnames(dt)[-1]
   
   # replace all missing values with the value 0.36 for braak soil
-  d1[,c(cols) := lapply(.SD,function(x) fifelse(is.na(x),0.23,x)),.SDcols = cols]
+  dt[,c(cols) := lapply(.SD,function(x) fifelse(is.na(x),0.23,x)),.SDcols = cols]
   
   # write file to data
-  crops.makkink <- copy(d1)
+  crops.makkink <- copy(dt)
   save(crops.makkink, file = 'data/crops_makkink.RData')
     
