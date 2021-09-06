e819c6a87509dd832adc99039039f5c7ab3b2f80
Gerard <gerard.ros@nmi-agro.nl>
2021-09-06 11:40:39 +0200

    update binnenveld met zcrit, glg, ghg

--- DEV/scripts/ppr_binnenveld.R
+++ DEV/scripts/ppr_binnenveld.R
@@ -6,5 +6,8 @@
   # read in the csv file with soil properties
   binnenveld <- fread('dev/data/binnenveld.csv')
   
+  # add B_GWL_GLG, B_GWL_GHG, and B_GWL_ZCRIT
+  
+  
   # save file
   save(binnenveld, file = 'data/binnenveld.RData')
