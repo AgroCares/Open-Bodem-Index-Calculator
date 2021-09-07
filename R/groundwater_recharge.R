b792328df8076bca240465a759c609782ddcae96
Gerard <gerard.ros@nmi-agro.nl>
2021-09-07 02:33:40 +0200

    change B_DRAINAGE in B_DRAIN

--- R/groundwater_recharge.R
+++ R/groundwater_recharge.R
@@ -7,25 +7,25 @@
 #' @param D_PSP (numeric) The precipitation surplus per crop  calculated by \code{\link{calc_psp}}
 #' @param I_P_SE (numeric) The indicator value for soil sealing
 #' @param I_P_CO (numeric) The indicator value for occurrence of subsoil compaction
-#' @param B_DRAINAGE (boolean) Are drains installed to drain the field (options: yes or no)
+#' @param B_DRAIN (boolean) Are drains installed to drain the field (options: yes or no)
 #' 
 #'         
 #' @export
-ind_gw_recharge <- function(D_WRI_WHC, D_PSP, I_P_SE, I_P_CO, B_DRAINAGE = FALSE){
+ind_gw_recharge <- function(D_WRI_WHC, D_PSP, I_P_SE, I_P_CO, B_DRAIN = FALSE){
   
   I_WRI_WHC = I_W_GWS = cf_compaction = cf_drain = D_I_PSP = NULL
   
   # Check inputs
-  arg.length <- max(length(I_P_CO),length(B_DRAINAGE))
+  arg.length <- max(length(I_P_CO),length(B_DRAIN))
   
   checkmate::assert_numeric(I_P_CO, any.missing = FALSE, len = arg.length)
-  checkmate::assert_logical(B_DRAINAGE, any.missing = FALSE, len = arg.length)
+  checkmate::assert_logical(B_DRAIN, any.missing = FALSE, len = arg.length)
   
   # import data into table
   dt <- data.table(D_WRI_WHC = D_WRI_WHC,
                    I_P_SE = I_P_SE,
                    I_P_CO = I_P_CO,
-                   B_DRAINAGE = B_DRAINAGE)
+                   B_DRAIN = B_DRAIN)
   
   # Calculate the indicator value for water holding capacity
   dt[,I_WRI_WHC := ind_waterretention(D_WRI_WHC,type = 'water holding capacity')]
@@ -34,8 +34,8 @@ ind_gw_recharge <- function(D_WRI_WHC, D_PSP, I_P_SE, I_P_CO, B_DRAINAGE = FALSE
   dt[,D_I_PSP := evaluate_logistic(D_PSP,0.05,300,2.5)]
   
   # Correct for subsoil compaction or drainage
-  dt[B_DRAINAGE == TRUE, c('cf_drain','cf_compaction') := list(0.6,1)]
-  dt[B_DRAINAGE == FALSE, c('cf_drain','cf_compaction') := list(1,fifelse(I_P_CO < 0.5,0.8,1))]
+  dt[B_DRAIN == TRUE, c('cf_drain','cf_compaction') := list(0.6,1)]
+  dt[B_DRAIN == FALSE, c('cf_drain','cf_compaction') := list(1,fifelse(I_P_CO < 0.5,0.8,1))]
   
   # Calculate aggregated score
   dt[,I_W_GWS := (0.35 * I_WRI_WHC + 0.35 * D_I_PSP + 0.3 * I_P_SE) * cf_compaction * cf_drain]
--- tests/testthat/test-groundwater_recharge.R
+++ tests/testthat/test-groundwater_recharge.R
@@ -5,7 +5,7 @@ test_that("whether ind_gw_recharge works", {
       I_P_CO = c(0.1,1,0.1,1,0.1,1),
       I_P_SE = rep(0.8,6),
       D_WRI_WHC =  c(0.1,0.25,.35,0.5,0.75,1),
-      B_DRAINAGE = rep(TRUE,6)
+      B_DRAIN = rep(TRUE,6)
     ),
     expected = c(0.15,0.216,0.411,0.532,0.561,0.564),
     tolerance = 0.01
