0982d0ef6a9d8ebde75127f58eb219a15c01571d
KeesDoolNMI <kees.vandendool@nmi-agro.nl>
2021-11-16 16:03:38 +0100

    Update tests

--- tests/testthat/test-groundwater_recharge.R
+++ tests/testthat/test-groundwater_recharge.R
@@ -2,12 +2,13 @@ test_that("whether ind_gw_recharge works", {
   expect_equal(
     ind_gw_recharge(
       D_PSP = c(100,200,300,400,500,600),
-      I_P_CO = c(0.1,1,0.1,1,0.1,1),
+      D_WRI_K = c(50,90,50,100,50,90),
       I_P_SE = rep(0.8,6),
-      D_WRI_WHC =  c(0.1,0.25,.35,0.5,0.75,1),
-      B_DRAIN = rep(TRUE,6)
+      I_P_CO = c(0.1,1,0.1,1,0.1,1),
+      B_DRAIN = rep(TRUE,6),
+      B_GWL_CLASS = c('GtIII','GtIII','GtIII','GtIV','GtIII','GtIV')
     ),
-    expected = c(0.15,0.216,0.411,0.532,0.561,0.564),
+    expected = c( 0.1274698, 0.3502220, 0.5416138, 0.5768809, 0.6772030, 0.5734448),
     tolerance = 0.01
   )
  
---
+++ tests/testthat/test-precipitation_surpluss.R
@@ -0,0 +1,27 @@
+test_that("calc_psp works", {
+  expect_equal(
+    calc_psp(c(233,256,266,259,2014),rep(FALSE,5)
+    ),
+    expected = c(417.70, 413.40, 209.48, 354.15, 387.19),
+    tolerance = 0.01
+  )
+  
+  expect_equal(
+    calc_psp(c(233,256,266,259,2014),rep(TRUE,5)
+    ),
+    expected = c(396.38, 370.14, 239.70, 332.21, 387.19),
+    tolerance = 0.01
+  )
+})
+
+
+test_that("ind_psp works", {
+  expect_equal(
+    ind_psp(c(100,200,300,350,400)
+    ),
+    expected = c(0.018, 0.135, 0.758, 0.969, 0.997),
+    tolerance = 0.001
+  )
+  
+  
+})
