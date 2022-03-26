cab513d04a7efec574e0fede6c5da462cd2af24b
Gerard <gerard.ros@nmi-agro.nl>
2022-03-26 12:56:28 +0100

    add examples and return to bodemconditiescore.R

--- R/bodemconditiescore.R
+++ R/bodemconditiescore.R
@@ -24,9 +24,16 @@
 #' 
 #' @import data.table
 #' 
+#' @examples
+#' calc_bcs(B_LU_BRP = 265, B_SOILTYPE_AGR = 'dekzand', A_SOM_LOI = 3.5, D_PH_DELTA = 0.4,
+#' A_EW_BCS = 1, A_SC_BCS = 1, A_GS_BCS = 1, A_P_BCS = 1, A_C_BCS = 1, A_RT_BCS =1, A_RD_BCS = 1, 
+#' A_SS_BCS = 1, A_CC_BCS = 1)
+#' 
+#' @return
+#' A visual soil assessment score derived from field observations driven by organic matter content and soil structure properties. Returns a numeric value.
 #' @export
 calc_bcs <- function(B_LU_BRP,B_SOILTYPE_AGR,A_SOM_LOI, D_PH_DELTA,
-                     A_EW_BCS = NA, A_SC_BCS = NA, A_GS_BCS = NA, A_P_BCS = NA, A_C_BCS = NA, A_RT_BCS =NA, A_RD_BCS = NA, 
+                     A_EW_BCS = NA, A_SC_BCS = NA, A_GS_BCS = NA, A_P_BCS = NA, A_C_BCS = NA, A_RT_BCS = NA, A_RD_BCS = NA, 
                      A_SS_BCS = NA, A_CC_BCS = NA,
                      type = 'score'
                      ) {
@@ -152,6 +159,13 @@ calc_bcs <- function(B_LU_BRP,B_SOILTYPE_AGR,A_SOM_LOI, D_PH_DELTA,
 #' This function calculates the final score for the BodemConditieScore by using the scores calculated by \code{\link{calc_bcs}}
 #' 
 #' @param D_BCS (numeric) The value of BCS  calculated by \code{\link{calc_bcs}}
+#' 
+#' @examples 
+#' ind_bcs(D_BCS = 12)
+#' ind_bcs(D_BCS = c(12,18,26,30))
+#' 
+#' @return 
+#' The evaluated score for the Visual Soil Assessment. A numeric value between 0 and 50.
 #' 
 #' @export
 ind_bcs <- function(D_BCS) {
