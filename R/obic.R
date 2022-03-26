a660a6a189eeac9a4a599977ecb8409fff8ca531
Gerard <gerard.ros@nmi-agro.nl>
2022-03-26 20:27:46 +0100

    remove visual bindings on OBIC tables in obic_field

--- R/obic.R
+++ R/obic.R
@@ -119,9 +119,9 @@ obic_field <- function(B_SOILTYPE_AGR,B_GWL_CLASS,B_SC_WENR,B_HELP_WENR,B_AER_CB
   I_P_DS = I_P_WS = I_P_CEC = D_P_CEC= I_P_WO = I_E_NGW = I_E_NSW = NULL
   D_M_SOILFERTILITY = D_M_CLIMATE = D_M_WATERQUALITY = D_M_BIODIVERSITY = NULL
   I_M_SOILFERTILITY = I_M_CLIMATE = I_M_WATERQUALITY = I_M_BIODIVERSITY = NULL
-  crop_category = crops.obic = leaching_to = NULL
+  crop_category = leaching_to = NULL
   
-  crop_code = weight.obic = weight = score.cf = . = out.ind = NULL
+  crop_code = weight = score.cf = . = out.ind = NULL
   weight_peat = weight_nonpeat = variable = NULL
   indicator = ind.n = value = value.w = value.cf = year.cf = value.group = value.year = NULL
   var = cf = ncat = id = NULL
