ca4158984ed1a0a2455207dd43b7be748d13d4e8
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2023-10-19 14:09:36 +0200

    Fix merge issues

--- DEV/scripts/ppr_crops_crumbleability.R
+++ DEV/scripts/ppr_crops_crumbleability.R
@@ -1,7 +1,7 @@
 # ppr_eval_crumbleability
 
 # read in csv file
-eval_crumbleability <- fread('dev/data/crumbleability.csv',sep=';')
+eval.crumbleability <- fread('dev/data/crumbleability.csv',sep=';')
 
 # save file
-usethis::use_data(eval_crumbleability, version = 3, overwrite = TRUE, compress = 'xz')
+usethis::use_data(eval.crumbleability, version = 3, overwrite = TRUE, compress = 'xz')
