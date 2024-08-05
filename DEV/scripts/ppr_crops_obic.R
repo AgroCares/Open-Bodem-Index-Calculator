af252df57b41bee8bb304eb2acfdbfd33b8d3d67
Brent <72964508+BrentWHR@users.noreply.github.com>
2024-08-05 11:42:33 +0200

    update crops.obic

--- DEV/scripts/ppr_crops_obic.R
+++ DEV/scripts/ppr_crops_obic.R
@@ -4,10 +4,9 @@
   library(data.table)
 
   # Load crops.obic (copied csv from pandex)
-  cr <- fread('DEV/data/crops_obic.csv')
+  crops.obic <- fread('data-raw/crops_obic/crops_obic.csv')
 
   # update the Rdata file
-  crops.obic <- copy(cr[, 2:ncol(cr)])
   usethis::use_data(crops.obic,version = 3, overwrite = TRUE, compress = 'xz')
   devtools::document()
   
