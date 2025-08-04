00610f889a2560d01150761b49337fedd42ae8c9
Brent <72964508+BrentWHR@users.noreply.github.com>
2025-08-04 10:37:31 +0200

    update crops.obic for BRP 2025

--- DEV/scripts/ppr_crops_obic.R
+++ DEV/scripts/ppr_crops_obic.R
@@ -9,4 +9,5 @@
   # update the Rdata file
   usethis::use_data(crops.obic,version = 3, overwrite = TRUE, compress = 'xz')
   devtools::document()
+  fwrite(crops.obic, 'data-raw/crops_obic/crops_obic.csv')
   
