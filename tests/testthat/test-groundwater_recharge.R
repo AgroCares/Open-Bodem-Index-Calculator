f0deeb90602e974ac6bf696d973a549928b9fe09
BrentWHR <BrentWHR@users.noreply.github.com>
2025-11-21 11:32:36 +0100

    adjust gw recharge unit tests accordingly (note 0.8 was an invalid I_P_SE value for grassland)

--- tests/testthat/test-groundwater_recharge.R
+++ tests/testthat/test-groundwater_recharge.R
@@ -4,40 +4,38 @@ test_that("whether ind_gw_recharge works", {
       B_LU_BRP = c(265,265,265,233,233,233),
       D_PSP = c(100,200,300,400,500,600),
       D_WRI_K = c(50,90,50,100,50,90),
-      I_P_SE = rep(0.8,6),
-      I_P_CO = c(0.1,1,0.1,1,0.1,1),
       B_DRAIN = c(TRUE,FALSE,TRUE,FALSE,TRUE,FALSE),
-      B_GWL_CLASS = c('III','III','III','IV','III','IV')
+      B_GWL_CLASS = c('III','III','III','IV','III','IV'),
+      B_SC_WENR = rep(c('Zeer groot', 'Zeer beperkt'), 3),
+      D_SE = c(1, 1, 1, 6.049445, 6.049445, 6.049445)
     ),
-    expected = c(0.1274698, 0.3502220, 0.5416138, 0.9614681, 0.6772030, 0.9557414),
+    expected = c(0.1514698, 0.3802220, 0.5656138, 0.9614681, 0.6772030, 0.9557414),
     tolerance = 0.01
   )
  
 })
 
 test_that("B_DRAIN affects scores output for IIIb and IV", {
-  expect_false(
-    all(
+  expect_all_false(
+    ind_gw_recharge(
+      B_LU_BRP = c(265,265,265,233,233,233),
+      D_PSP = c(100,200,300,400,500,600),
+      D_WRI_K = c(50,90,50,100,50,90),
+      B_DRAIN = rep(TRUE, 6),
+      B_GWL_CLASS = c('IIIb','IIIb','IIIb','IV','IIIb','IV'),
+      B_SC_WENR = rep(c('Zeer groot', 'Zeer beperkt'), 3),
+      D_SE = c(1, 1, 1, 6.049445, 6.049445, 6.049445)
+    ) ==
       ind_gw_recharge(
         B_LU_BRP = c(265,265,265,233,233,233),
         D_PSP = c(100,200,300,400,500,600),
         D_WRI_K = c(50,90,50,100,50,90),
-        I_P_SE = rep(0.8,6),
-        I_P_CO = c(0.1,1,0.1,1,0.1,1),
-        B_DRAIN = rep(TRUE, 6),
-        B_GWL_CLASS = c('IIIb','IIIb','IIIb','IV','IIIb','IV')
-      ) ==
-        ind_gw_recharge(
-          B_LU_BRP = c(265,265,265,233,233,233),
-          D_PSP = c(100,200,300,400,500,600),
-          D_WRI_K = c(50,90,50,100,50,90),
-          I_P_SE = rep(0.8,6),
-          I_P_CO = c(0.1,1,0.1,1,0.1,1),
-          B_DRAIN = rep(FALSE, 6),
-          B_GWL_CLASS = c('IIIb','IIIb','IIIb','IV','IIIb','IV')
+        B_DRAIN = rep(FALSE, 6),
+        B_GWL_CLASS = c('IIIb','IIIb','IIIb','IV','IIIb','IV'),
+        B_SC_WENR = rep(c('Zeer groot', 'Zeer beperkt'), 3),
+        D_SE = c(1, 1, 1, 6.049445, 6.049445, 6.049445)
         )
-    )
-  )
+      )
 })
 
 test_that("Groundwater recharge is correctly modified with a target by ind_gw_target", {
@@ -66,4 +64,60 @@ test_that("Groundwater recharge is correctly modified with a target by ind_gw_ta
   expect_gt(dt[B_SOILTYPE_AGR == 'loess' & B_GWL_CLASS == 'IIa', S_H_GWR], 
             dt[B_SOILTYPE_AGR == 'loess' & B_GWL_CLASS == 'VIIIo', S_H_GWR])
 
+})
+
+test_that('Appropriate warnings are given for deprecated arguments',{
+  expect_warning(
+    ind_gw_recharge(
+      B_LU_BRP = 265,
+      D_PSP = 400,
+      D_WRI_K = 50,
+      I_P_SE = 0.8,
+      B_DRAIN = TRUE, 
+      B_GWL_CLASS = 'IIIb',
+      B_SC_WENR = 'Matig'
+    )
+  )
+  
+  expect_warning(
+    ind_gw_recharge(
+      B_LU_BRP = 265,
+      D_PSP = 400,
+      D_WRI_K = 50,
+      I_P_CO = 0.1,
+      B_DRAIN = TRUE, 
+      B_GWL_CLASS = 'IIIb',
+      D_SE = 25
+    )
+  )
+
+  expect_error(
+    expect_warning(
+      ind_gw_recharge(
+        B_LU_BRP = 265,
+        D_PSP = 400,
+        D_WRI_K = 50,
+        B_DRAIN = TRUE,
+        I_P_SE = 0.8,
+        B_GWL_CLASS = 'IIIb',
+        B_SC_WENR = 'Matig',
+        D_SE = 25
+      )
+    ) 
+  )
+  
+  expect_error(
+    expect_warning(
+      ind_gw_recharge(
+        B_LU_BRP = 265,
+        D_PSP = 400,
+        D_WRI_K = 50,
+        B_DRAIN = TRUE,
+        I_P_CO = 0.1,
+        B_GWL_CLASS = 'IIIb',
+        B_SC_WENR = 'Matig',
+        D_SE = 25
+      )
+    ) 
+  )
 })
