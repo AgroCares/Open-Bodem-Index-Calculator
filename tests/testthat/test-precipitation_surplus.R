a2e427f41e2dd68623f4da14bb6963026a620f03
BrentWHR <BrentWHR@users.noreply.github.com>
2025-11-26 13:37:06 +0100

    Use simplified psp calculation

--- tests/testthat/test-precipitation_surplus.R
+++ tests/testthat/test-precipitation_surplus.R
@@ -4,7 +4,7 @@ test_that("whether calc_psp works", {
       B_LU_BRP = c(265, 265, 266, 266, 235, 235),
       M_GREEN = c(TRUE, FALSE, TRUE, FALSE, TRUE, FALSE)
     ),
-    expected = c(220.993, 228.180, 220.993, 246.870, 403.549, 417.695),
+    expected = c(209.475, 209.475, 209.475, 209.475, 400.749, 417.695),
     tolerance = 0.01
   )
 })
@@ -13,15 +13,51 @@ test_that("calc_psp works", {
   expect_equal(
     calc_psp(c(233,256,266,259,2014),rep(FALSE,5)
     ),
-    expected = c(417.695, 413.397, 209.475, 375, 430),
+    expected = c(417.695, 413.397, 209.475, 375.46, 430.45),
     tolerance = 0.01
   )
   
   expect_equal(
     calc_psp(c(233,256,266,259,2014),rep(TRUE,5)
     ),
-    expected = c(396.377, 370.138, 239.698, 332.205, 387.193),
+    expected = c(400.75, 374.510, 209.475, 336.577, 391.565),
     tolerance = 0.01
+  )
+})
+
+test_that("calc_psp() works without start and termination arguments", {
+  expect_equal(
+    object = calc_psp(B_LU_BRP = 2014, M_GREEN = TRUE),
+    expected = 410,
+    tolerance = 1
+  )
+  # two years 
+  expect_equal(
+    object = calc_psp(B_LU_BRP = c(2014, 233), M_GREEN = c(TRUE, FALSE)),
+    expected = c(410, 417),
+    tolerance = 1
+  )
+  # two fields
+  expect_equal(
+    object = calc_psp(B_LU_BRP = c(2014, 233), M_GREEN = c(TRUE, FALSE)),
+    expected = c(410, 417),
+    tolerance = 1
+  )
+})
+
+test_that("calc_psp() works with specified green manure start and termination dates", {
+  expect_equal(
+    object = calc_psp(B_LU_BRP = 2014, M_GREEN = TRUE, 
+                           M_GREEN_START = 9L, M_GREEN_TERMINATE = 3L),
+    expected = 384,
+    tolerance = 1
+  )
+  
+  expect_equal(
+    object = calc_psp(B_LU_BRP = c(6794, 6794) , M_GREEN = c(TRUE, FALSE), 
+                           M_GREEN_START = 8L, M_GREEN_TERMINATE = 4L),
+    expected = c(573, 645),
+    tolerance = 1
   )
 })
 
